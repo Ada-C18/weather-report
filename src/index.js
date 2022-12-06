6f1081c25c28a9f7319902ee239a155de5336b47
Margaret <y.margaret.lu+github@gmail.com>
2022-12-05 20:04:36 -0800

    made the temp a color

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
 "use strict";
+
+const currentTemp = {
+    currTemp: 72, //This will be a function call in later wave
+  };
+
+const upTemp = () => {
+    console.log('up')
+    currentTemp.currTemp++;
+    const tempContainer = document.querySelector("#temp-current");
+    tempContainer.textContent = `${currentTemp.currTemp}`;
+    tempColor(currentTemp.currTemp)
+  };
+
+  const downTemp = () => {
+    console.log('down')
+    currentTemp.currTemp--;
+    const tempContainer = document.querySelector("#temp-current");
+    tempContainer.textContent = `${currentTemp.currTemp}`;
+    tempColor(currentTemp.currTemp)
+  };
+
+/*
+  | 80+             | Red    |
+  | 70-79           | Orange |
+  | 60-69           | Yellow |
+  | 50-59           | Green  |
+  | 49 or below     | Teal   |
+*/
+
+  const tempColor = () => {
+    const tempCurrent = document.getElementById('temp-current')
+    const numericTemp = parseInt(tempCurrent.innerHTML)
+    let color;
+    switch (true) {
+        case (numericTemp >= 80):
+            color = 'red';
+            break;
+        case (numericTemp >= 70):
+            color = 'orange';
+            break;
+        case (numericTemp >= 60):
+            color = 'yellow';
+            break;
+        case (numericTemp >= 50):
+            color = 'green';
+            break;
+        default:
+            color = 'teal';
+            break;
+    }
+    tempCurrent.className = color;
+    }
+    
+  
+  const registerEventHandlers = () => {
+    const increaseTemp = document.getElementById("temp-up");
+    increaseTemp.addEventListener("click", upTemp);
+    
+    const decreaseTemp = document.getElementById("temp-down");
+    decreaseTemp.addEventListener("click", downTemp);
+    
+  };
+  
+  document.addEventListener("DOMContentLoaded", registerEventHandlers);
