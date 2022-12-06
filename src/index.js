c25fe3552e7e3fd79cb002c9777cb8affcc2de45
Dominique <dominiquedaba@gmail.com>
2022-12-06 09:57:56 -0800

    implemented down button, up button still works

--- src/index.js
+++ src/index.js
@@ -8,10 +8,19 @@ const addTemp = (event) => {
   tempControl.textContent = state.defaultTemp;
 };
 
+const minusTemp = (event) => {
+  state.defaultTemp -= 1;
+  const tempControl = document.querySelector('#currentTemp');
+  tempControl.textContent = state.defaultTemp;
+};
+
 const registerEventHandlers = (event) => {
+  const decreaseTempButton = document.querySelector('#decreaseTemp');
+  decreaseTempButton.addEventListener('click', minusTemp);
+
   const increaseTempButton = document.querySelector('#increaseTemp');
   increaseTempButton.addEventListener('click', addTemp);
-  console.log('click!');
+  console.log('click click!!');
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
