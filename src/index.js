54c61dae400441746f160f5ef1b520aa7f77c160
Maxwell <maxpasqualis@gmail.com>
2022-12-06 11:07:18 -0800

    added temperature incrementing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-"use strict"
-const state ={
-  temputure : 70,
-  landScape:"----"
-}
+'use strict';
+const state = {
+  temperature: 70,
+  landScape: '----',
+};
 
+const updateTemperature = () => {
+  const tempDisplay = document.getElementById('temp-display');
+  tempDisplay.textContent = `${state.temperature} degrees`;
+};
 
+const incrementTemperature = (type) => {
+  if (type === 'add') {
+    state.temperature += 1;
+  }
+  if (type === 'subtract') {
+    state.temperature -= 1;
+  }
+  updateTemperature();
+};
 
-let tempDisplay= document.getElementById("temp-display")
+const registerEventHandlers = () => {
+  updateTemperature();
+  const tempUpButton = document.getElementById('temp-up-btn');
+  tempUpButton.addEventListener('click', function () {
+    incrementTemperature('add');
+  });
 
-const registerEventHanlers=()=>{
-  const tempUp= document.getElementById("temp-up-btn")
-  tempUp.addEventListener("click")
-  
-  let tempDown= document.getElementById("temp-down-btn")
-  tempDown.addEventListener("click")
-}
+  const tempDownButton = document.getElementById('temp-down-btn');
+  tempDownButton.addEventListener('click', function () {
+    incrementTemperature('subtract');
+  });
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
