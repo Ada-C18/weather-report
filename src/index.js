96a0a0dcd292a2edb30c996e7725673d8ace7135
Samantha Hunt <ariaandsam@gmail.com>
2022-12-07 14:04:00 -0800

    rearrange variables

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-// Todays Goals:
-// Have clickable buttons for temperature
-// Change color of temperature according to wave-02.md
-// Change landscape according to wave-02.md
-
-// getElementById for the buttons and the temp
-// Add an event listener ( onClick )for both buttons ( ^ increases by 1, v decreases by 1 )
-// Add an event listener that checks temp and assigns color accordingly
-
 let temperature = document.getElementById("display-temp")
 
-//Set default temp (will update with Weather API using proxy server)
 window.onload = () => {temperature.innerHTML = 50}
 
-//Temp Buttons:
+const landscape = document.getElementById("ground-emoji")
 const tempUp = document.getElementById("temp-up")
 const tempDown = document.getElementById("temp-down")
+
+let inputCity = document.getElementById("input-city")
+let cityName = document.getElementById("city-name")
+
 tempUp.addEventListener('click', () => {
     let tempHTML = Number(temperature.innerHTML)
     tempHTML += 1;
@@ -28,7 +22,6 @@ tempDown.addEventListener('click', () => {
     changeColorAndEmojis(temperature)
 })
 
-const landscape = document.getElementById("ground-emoji")
 const changeColorAndEmojis = (temperature) => {
 
     if (temperature.innerText >= 80) {
@@ -56,23 +49,8 @@ const changeColorAndEmojis = (temperature) => {
         temperature.style.backgroundColor = 'white'
         landscape.innerText = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲'
     }
-    // switch(Number(temperature.innerHTML)){
-    //     case Number(temperature.innerHTML) >= 80:
-    //         temperature.style.color = 'red'
-    //         temperature.style.backgroundColor = 'red'
-    //         landscape.innerText = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂'
-    //         break;
-    //     case Number(temperature.innerHTML) >= 70:
-    //         temperature.style.color = 'orange' 
-    //         temperature.style.backgroundColor = 'orange'
-    //         landscape.innerText = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷'
-    //         break;
-    // }
 }
 
-let inputCity = document.getElementById("input-city")
-
-let cityName = document.getElementById("city-name")
 const changeCity = (inputCity) => {
     cityName.innerHTML = inputCity.value
 }
