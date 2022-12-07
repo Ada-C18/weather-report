7df5ca3a0602b496e4d0740f6f6266b653d4a679
Melley Gebretatios <gmelley4@gmail.com>
2022-12-07 18:03:28 -0500

    Wave2 complete
    Co-authored-by: allcomputersarebad <allcomputersarebad@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 'use strict';
+
+let temperature = 0;
+
+const increaseTemp = () => {
+  temperature++;
+  updatePage();
+};
+
+const decreaseTemp = () => {
+  temperature--;
+  updatePage();
+};
+
+const updatePage = () => {
+  document.getElementById('temp').textContent = temperature;
+  let newstyle = temperatureStyle(temperature);
+  document.getElementById('body').style.backgroundColor = newstyle.bg_color;
+  document.getElementById('landscape').textContent = newstyle.landscape;
+}
+
+const temperatureStyle = (temp) => {
+  if (temp > 80) {
+    return { bg_color: 'red', landscape: '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂' };
+  } else if (temp >= 70) {
+    return { bg_color: 'orange', landscape: '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷' };
+  } else if (temp >= 60) {
+    return { bg_color: 'yellow', landscape: '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃' };
+  } else if (temp >= 50) {
+    return { bg_color: 'green', landscape: '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲' };
+  } else {
+    return { bg_color: 'teal', landscape: '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲' };
+  }
+};
