e2be6f108fdb3bc385e292b07ea80e6ef60f4855
Minh <minh.seikel@gmail.com>
2022-12-07 14:12:40 -0800

    adding landscape and logic to change landscape by temp changes

--- src/index.js
+++ src/index.js
@@ -17,21 +17,43 @@ const minusTemp = (event) => {
   changeTempNumColor();
 };
 
-//logic for temperture color to change
+//HELPER FUNCTION logic for temperture color to change
 const changeTempNumColor = () => {
   let temp = state.defaultTemp;
+  const skyElement = document.querySelector('#sky');
+  const landScapeElement = document.querySelector('#gardenlandscape');
+
   if (temp >= 80) {
     document.getElementById('currentTemp').className = 'red';
+    skyElement.textContent = '🔥🥵🌞🔥🥵🌞🔥🥵🌞';
+    landScapeElement.textContent = '👙🩳⛱🏝🌊👙🩳⛱🏝🌊👙🩳⛱🏝🌊'
   } else if (temp >= 70 && temp <= 79) {
     document.getElementById('currentTemp').className = 'orange';
+    skyElement.textContent = '🌤☁️🌤☁️🌤☁️🌤☁️🌤☁️';
+    landScapeElement.textContent = '🌻🌹🍉🌻🌹🍉🌻🌹🍉🌻🌹🍉🌻🌹🍉'
   } else if (temp >= 60 && temp <= 69) {
     document.getElementById('currentTemp').className = 'yellow';
+    skyElement.textContent = '☁️🌥☁️🌥☁️🌥☁️🌥☁️🌥';
+    landScapeElement.textContent = '🌧☔️🌈🌧☔️🌈🌧☔️🌈🌧☔️🌈'
   } else if (temp >= 50 && temp <= 59) {
     document.getElementById('currentTemp').className = 'green';
+    skyElement.textContent = '☁️☁️☁️☁️🌥☁️☁️☁️☁️';
+    landScapeElement.textContent = '🍁🍂🌲🌳🍁🍂🌲🌳🍁🍂🌲🌳'
   } else {
     document.getElementById('currentTemp').className = 'teal';
+    skyElement.textContent = '☁️🌧❄️☁️🌧❄️☁️🌧❄️☁️🌧❄️';
+    landScapeElement.textContent = '☃️🧤🥶🏔☃️🧤🎄🥶🏔☃️🧤🥶🏔'
   }
 };
+
+//HELPER FUNCTION TO CHANGE SKY AND LANDSCAPE
+
+// const changeSkyAndLandscape = () =>{
+//   const skyElement = document.querySelector('#sky');
+//   const landScapeElement = document.querySelector('#landscape');
+
+//   let temp = 
+// }
 
 // event handlers and listeners
 const registerEventHandlers = (event) => {
