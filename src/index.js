cd088ec4d7dde2a88c120e950d64403b3f16350e
Shweet Abraha <sabraha0311@gmail.com>
2022-12-07 15:22:30 -0800

    Temp color changes code in process.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 'use strict';
 
-{
-  /* <script>
-  var i = 0; function buttonClick(){' '}
-  {(document.getElementById('inc').value = ++i)}
-</script>; */
-}
-
 function incrementButton() {
   console.log('hello world');
   var element = document.getElementById('counter');
@@ -26,4 +19,20 @@ function decreaseButton() {
 
   console.log(value);
   document.getElementById('counter').innerHTML = value;
+}
+
+const state = { temperature: temperature };
+
+for (let temp of state.temperature) {
+  if (temp >= 80) {
+    let red = document.getElementById('red');
+  } else if (temp >= 70 && temp <= 79) {
+    let orange = document.getElementById('orange');
+  } else if (temp >= 60 && temp <= 69) {
+    let yellow = document.getElementById('yellow');
+  } else if (temp >= 50 && temp <= 59) {
+    let green = document.getElementById('green');
+  } else if (temp <= 49) {
+    let teal = document.getElementById('teal');
+  }
 }
