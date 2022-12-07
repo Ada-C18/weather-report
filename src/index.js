07586b9f1e32d6596138ed6e87237155130a3866
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-12-07 15:29:37 -0800

    changeColor based on temperature function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 let counterDisplayElem = document.querySelector('.count');
 let counterMinusElem = document.querySelector('.counter-minus');
 let counterPlusElem = document.querySelector('.counter-plus');
+let count = 0;
+
+// Temperature
+const changeColor = (obj) => {
+  if (obj.value < 50) {
+    // obj.cold = 'teal';
+    obj.style.backgroundColor = 'teal';
+    console.log('color changed');
+  } else if (obj.value < 60) {
+    obj.chilly = 'green';
+  } else if (obj.value > 70) {
+    obj.warm = 'yellow';
+  } else if (obj.value < 80) {
+    obj.hot = 'orange';
+  } else if (obj.value > 80) {
+    obj.veryHot = 'red';
+  }
+};
+
 // City Search Bar
 const inputSubmitButton = document.getElementById('submit-button');
-
-let count = 0;
 
 const registerEventHandlers = () => {
   counterPlusElem.addEventListener('click', () => {
     counterDisplayElem.innerHTML++;
+    console.log('clicked plus');
+    changeColor(counterDisplayElem);
+    console.log('color changed');
   });
 
   counterMinusElem.addEventListener('click', () => {
     counterDisplayElem.innerHTML--;
+    changeColor(counterDisplayElem);
   });
 
   // City Search Bar
