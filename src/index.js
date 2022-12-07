667d9831549b1d206a5b3a8bd59fb8bdb15f93fb
Jennifer <datateur@gmail.com>
2022-12-07 13:15:11 -0800

    Created img folder and added images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 "use strict";
-const axios = require('axios');
+//const axios = require('axios');
 
+const state = {
+  tempValue: 0,
+};
+
+const temperature = document.getElementById('tempValue');
+
+const increaseTemp = () => {
+  state.tempValue += 1;
+  temperature.textContent = String(state.tempValue) + '°F';
+};
+
+const decreaseTemp = () => {
+  state.tempValue -= 1;
+  temperature.textContent = String(state.tempValue +'°F');
+};
+
+const registerEventHandlers = () => {
+  const increaseTempBtn = document.getElementById('increaseTempBtn');
+  increaseTempBtn.addEventListener('click', increaseTemp);
+
+  const decreaseTempBtn = document.getElementById('decreaseTempBtn');
+  decreaseTempBtn.addEventListener('click', decreaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
