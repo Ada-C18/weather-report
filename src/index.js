03f71688473f065bc076338864a13394c5ddc74b
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-12-07 15:22:29 -0800

    Remove print statements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 'use strict';
 let temperature = 50;
 
+// Increase temperature
 const increaseTemp = () => {
-  // let arrowDown = document.querySelector('#arrow-down');
   const spanTempNumber = document.getElementById('temperature-value');
-
   temperature += 1;
   changeLandscape();
   changeTempNumberColor();
   spanTempNumber.textContent = `${temperature}`;
-  // else temperature -= 1;
   console.log('inside of increase', spanTempNumber.textContent);
 };
 
@@ -18,15 +16,13 @@ const increaseTempOnClick = () => {
   arrowUp.addEventListener('click', increaseTemp);
 };
 
-// decrease temp
+// Decrease temperature
 const decreaseTemp = () => {
   const spanTempNumber = document.getElementById('temperature-value');
-
   temperature -= 1;
   changeLandscape();
   changeTempNumberColor();
   spanTempNumber.textContent = `${temperature}`;
-
   console.log('inside of decrease', spanTempNumber.textContent);
 };
 
@@ -39,19 +35,12 @@ const changeTempNumberColor = () => {
   const tempNumber = Number(
     document.getElementById('temperature-value').innerText
   );
-
   const spanTempNumber = document.getElementById('temperature-value');
-  // console.log(spanTempNumber)
-
-  // console.log(tempNumber)
-
   if (tempNumber <= 49) spanTempNumber.className = 'number-color-teal';
   else if (tempNumber < 59) spanTempNumber.className = 'number-color-green';
   else if (tempNumber < 69) spanTempNumber.className = 'number-color-yellow';
   else if (tempNumber < 79) spanTempNumber.className = 'number-color-orange';
   else spanTempNumber.className = 'number-color-red';
-
-  // console.log(tempNumber.className)
 };
 
 const changeLandscape = () => {
@@ -59,19 +48,12 @@ const changeLandscape = () => {
     document.getElementById('temperature-value').innerText
   );
   let iconGround = document.getElementById('ground-icons');
-  // console.log(iconGround);
-
-  // console.log(tempNumber);
-
   if (tempNumber <= 59)
     iconGround.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
   else if (tempNumber < 69) iconGround.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
   else if (tempNumber < 79) iconGround.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
   else iconGround.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
 };
-// changeTemp();
-// changeLandscape();
-// changeTempNumberColor();
-// changeTempOnClick();
+
 document.addEventListener('DOMContentLoaded', increaseTempOnClick);
 document.addEventListener('DOMContentLoaded', decreaseTempOnClick);
