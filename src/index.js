d3515ff5b5b538d99c9152f7e26c1e8d60748b37
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2022-12-07 14:53:24 -0500

    Working on Wave 3 and 6

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 "use strict";
+const updateCityName = ()=>{
+  const chosenCityInputName = document.getElementById('cityNameInput').value;
+};
+
+const resetCityName = ()=>{
+  const originalNameInput = document.getElementById('cityNameInput');
+  updateCityName(); //callback function
+};
+
+const updateCrabCount = () => {
+  const crabCounterElement = document.querySelector('#crabCounter');
+  crabCounterElement.textContent = `Total Crab: ${state.crabCount}`;
+  
+};
+
+
+
+
+const registerEventHandlers= () =>{};
