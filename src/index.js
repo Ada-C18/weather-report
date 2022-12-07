38fd92d960720f0aef0f6f9f2343f40b6c233c2d
yangsu <suyang.sophia@outlook.com>
2022-12-07 15:22:29 -0800

    get backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-<script src="./node_modules/axios/dist/axios.min.js"></script>;
+console.log('Hello, World Fataneh!');
+
+const axios = require('axios');
+axios
+  .get('API url')
+  .then((response) => {
+    console.log('success!');
+  })
+  .catch(() => {
+    console.log('error!');
+  });
