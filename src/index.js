dd6be7330e815262bbe5fdf7aae4ff44e4d79b3a
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-12-07 14:32:00 -0800

    add city search bar to registered event handlers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 let counterDisplayElem = document.querySelector('.count');
 let counterMinusElem = document.querySelector('.counter-minus');
 let counterPlusElem = document.querySelector('.counter-plus');
+// City Search Bar
+const inputSubmitButton = document.getElementById('submit-button');
 
 let count = 0;
 
@@ -12,12 +14,12 @@ const registerEventHandlers = () => {
   counterMinusElem.addEventListener('click', () => {
     counterDisplayElem.innerHTML--;
   });
+
+  // City Search Bar
+  inputSubmitButton.addEventListener('click', () => {
+    console.log(document.getElementById('submit-city'));
+    console.log('you clicked submit');
+  });
 };
-
-//City Search Bar
-
-// document.getElementById('submit-button').addEventListener('click', () => {
-//   console.log(document.getElementById('submit-city'));
-// });
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
