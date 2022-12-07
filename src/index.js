9a5a4f195a2aab3c03b70f1e8f83fda834fab2db
Tara <talsaidi@ucdavis.edu>
2022-12-07 12:27:55 -0800

    wave 2: added temperature increase and decrease button functionality. created state variable, created event handlers for adding and removing single degree when click event is triggered, registered event handlers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// make body background reactive to state of temp counter
+
+const state = {
+  tempCount: 0,
+};
+
+const addDegree = () => {
+  state.tempCount += 1;
+  const tempCounter = document.querySelector('#tempCounter');
+  tempCounter.textContent = `${state.tempCount} \u00B0`;
+};
+const removeDegree = () => {
+  state.tempCount -= 1;
+  const tempCounter = document.querySelector('#tempCounter');
+  tempCounter.textContent = `${state.tempCount} \u00B0`;
+};
+
+const registerEventHandlers = () => {
+  const increaseTempButton = document.querySelector('#increaseTempButton');
+  increaseTempButton.addEventListener('click', addDegree);
+
+  const decreaseTempButton = document.querySelector('#decreaseTempButton');
+  decreaseTempButton.addEventListener('click', removeDegree);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
