8edda5fb8e09b0325c2dc257b6e7c0ffa3fc423e
Ariam Yonas <ariam.yonas@hotmail.de>
2022-12-07 14:31:02 -0800

    added decrease temo and change temp color using event listner

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
 const state = {
-  currentTemp: 108,
+  currentTemp: 79,
 };
 
 // temperature
-const changeTemp = () => {
+const increaseTemp = () => {
   state.currentTemp += 1;
   const tempContainer = document.querySelector('#currentTemp');
   tempContainer.textContent = `${state.currentTemp}`;
+};
+const decreaseTemp = () => {
+  state.currentTemp -= 1;
+  const tempContainer = document.querySelector('#currentTemp');
+  tempContainer.textContent = `${state.currentTemp}`;
+};
+const changeTempColor = () => {
+  const newLandscape = document.createElement('span');
+  const landscapeContainer = document.querySelector('#landscapeSection');
+  const box = document.getElementById('currentTemp');
+  if (state.currentTemp >= 80) {
+      box.style.color = 'red';
+      newLandscape.textContent = "🌵";
+      landscapeContainer.appendChild(newLandscape)
+
+  }else if ( state.currentTemp >= 70 && state.currentTemp <=79){
+    box.style.color = 'orange';
+  }else if ( state.currentTemp >= 60 && state.currentTemp <=69){
+    box.style.color = 'yellow';
+  }else if ( state.currentTemp >= 50 && state.currentTemp <=59){
+    box.style.color = 'green';
+  }else {
+    box.style.color = 'teal';
+  }
 };
 
 const registerEventHandlers = () => {
   const up = document.querySelector('#up');
   const down = document.querySelector('#down');
-  up.addEventListener('click', changeTemp);
+  up.addEventListener('click', increaseTemp);
   down.addEventListener('click', decreaseTemp);
+  up.addEventListener('click', changeTempColor);
+  down.addEventListener('click',  changeTempColor);
 };
 
 // reset the city temperature
