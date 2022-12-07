e55bfb4cf42ca8435f6bcc9fb915895a33b8819e
Tami <tamiadetayo@gmail.com>
2022-12-07 14:28:42 -0800

    Created increase and decrease temp functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const increaseTemp = () => {
+  const currentTemp = document.querySelector("#temp_value");
+  currentTemp++;
+}
+
+const decreaseTemp = () => {
+  const currentTemp = document.querySelector('#temp_value');
+  currentTemp--;
+}
+
+const registerEventHandlers = (event) => {
+  const increaseButton = document.querySelector("#increase_temp");
+  increaseButton.addEventListener('click', increaseTemp);
+  const decreaseButton = document.querySelector('#decrease_temp');
+  decreaseButton.addEventListener('click', decreaseTemp);
+}
+
+const setup = () => {
+  increaseTemp();
+  decreaseTemp();
+}
+
+if (document.readyState !== "loading") {
+  setup();
+} else {
+  document.addEventListener("DOMContentLoaded", setup);
+}
