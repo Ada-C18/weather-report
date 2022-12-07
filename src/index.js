9ecc9e465a8e51b0d4889b187aba2518a71a7239
mhc <mhchau0+ada@gmail.com>
2022-12-07 18:40:14 -0500

    fixed tempButtons index.js & script path

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 const state = {
-  Temperature: 0
+  Temperature: 0,
 };
 
 const increaseTemp = (event) => {
-  const tempContainer = document.querySelector("#Temperature");
-  tempContainer.textContent = `${state.Temperature + 1}`;
+  const tempContainer = document.querySelector('#Temperature');
+  console.log('increaseTemp clicked');
+  state.Temperature += 1;
+  tempContainer.textContent = `${state.Temperature}`;
 };
 
 const decreaseTemp = (event) => {
-  const tempContainer = document.querySelector("#Temperature");
+  const tempContainer = document.querySelector('#Temperature');
+  state.Temperature -= 1;
   tempContainer.textContent = `${state.Temperature - 1}`;
 };
 
 const registerEventHandlers = (event) => {
-  const increaseTempButton = document.querySelector("#increaseTempButton");
-  const decreaseTempButton = document.querySelector("#decreaseTempButton");
-  increaseTempButton.addEventListener("click", increaseTemp);
-  decreaseTempButton.addEventListener("click", decreaseTemp);
+  const increaseTempButton = document.querySelector('#increaseTempButton');
+  const decreaseTempButton = document.querySelector('#decreaseTempButton');
+  increaseTempButton.addEventListener('click', increaseTemp);
+  decreaseTempButton.addEventListener('click', decreaseTemp);
 };
 
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
