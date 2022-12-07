6df58d72eead112af84705747d72cde4064d6111
Melissa <ma.pena94@gmail.com>
2022-12-07 11:40:06 -0800

    Added function that changes the color of temp depending on a value

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 'use strict';
+
 const changeTempNumberColor = () => {
-  const tempNumber = document.getElementById('#temperature-value');
+  const tempNumber = Number(document.getElementById('temperature-value').innerText);
+
+  const spanTempNumber = document.getElementById('temperature-value')
+  console.log(spanTempNumber)
+
+  console.log(tempNumber)
+
+  if (tempNumber <= 49) spanTempNumber.className = "number-color-teal";
+
+  else if (tempNumber < 59 ) spanTempNumber.className = "number-color-green";
+
+  else if (tempNumber < 69 ) spanTempNumber.className = "number-color-yellow";
+
+  else if (tempNumber < 79 ) spanTempNumber.className = "number-color-orange";
+
+  else spanTempNumber.className = "number-color-red";
+
+  console.log(tempNumber.className)
+
 };
+
+changeTempNumberColor()
