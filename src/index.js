c324946b644f3852b587747fc67e2e326d98d185
Shweet Abraha <sabraha0311@gmail.com>
2022-12-07 12:11:25 -0800

    Added inc and dec buttons to html and JS files.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 'use strict';
+
+{
+  /* <script>
+  var i = 0; function buttonClick(){' '}
+  {(document.getElementById('inc').value = ++i)}
+</script>; */
+}
+
+function incrementButton() {
+  var element = document.getElementById('inc');
+  var value = element.innerHTML;
+
+  ++value;
+
+  console.log(value);
+  document.getElementById('inc').innerHTML = value;
+}
+
+function decreasButton() {
+  var element = document.getElementById('dec');
+  var value = element.innerHTML;
+
+  --value;
+
+  console.log(value);
+  document.getElementById('dec').innerHTML = value;
+}
