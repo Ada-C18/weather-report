ba67e55493a1077574cd34a52d637c7a3fef6b3f
Reyna Diaz <reynamdiazz@gmail.com>
2022-12-07 17:34:41 -0500

    "adds changeColor"

--- src/index.js
+++ src/index.js
@@ -12,12 +12,39 @@ temperatureValue.innerHTML = temp;
 const handleIncrement = () => {
   temp++;
   temperatureValue.innerHTML = temp;
+  changeColor();
 };
 
 const handleDecrement = () => {
   temp--;
   temperatureValue.innerHTML = temp;
+  changeColor();
 };
 
 incrementCount.addEventListener('click', handleIncrement);
 decrementCount.addEventListener('click', handleDecrement);
+
+const state = {
+  temp: 58,
+};
+
+const changeColor = () => {
+  let temp = state.temp;
+  let color = '';
+  if (temp > 80) {
+    color = 'red';
+  } else if (temp > 70) {
+    color = 'orange';
+  } else if (temp > 60) {
+    color = 'yellow';
+  } else if (temp > 50) {
+    color = 'green';
+  } else {
+    color = 'blue';
+  }
+  totalCount.className = color;
+};
+
+const registerEventHandlers = () => {
+  changeColor();
+};
