ebb465071a3a32802338e0c4752accfc87474cc3
Jennifer <datateur@gmail.com>
2022-12-07 15:36:17 -0800

    Created updateSky function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 "use strict";
-//const axios = require('axios');
+//import axios from 'axios';
 
 const state = {
   tempValue: 0,
@@ -17,12 +17,34 @@ const decreaseTemp = () => {
   temperature.textContent = String(state.tempValue +'°F');
 };
 
+const updateSky = () => {
+  const sky = document.getElementById('sky');
+  const skySelection = document.getElementById('skySelection').value;
+
+  if (skySelection === 'Sunny'){
+    sky.textContent = '☁️  🕊️☁️        ☀️ ☁️';
+  }
+  else if (skySelection === 'Cloudy'){
+    sky.textContent = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
+  }
+  else if (skySelection === 'Rainy'){
+    sky.textContent = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧';
+  }
+  else if (skySelection === 'Snowy'){
+    sky.textContent = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
+  }
+};
+
 const registerEventHandlers = () => {
   const increaseTempBtn = document.getElementById('increaseTempBtn');
   increaseTempBtn.addEventListener('click', increaseTemp);
 
   const decreaseTempBtn = document.getElementById('decreaseTempBtn');
   decreaseTempBtn.addEventListener('click', decreaseTemp);
+
+  const skySelection = document.getElementById('skySelection');
+  skySelection.addEventListener('change', updateSky);
+
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
