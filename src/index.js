d1835b0cc9e179ddef558485b9dcab1b08138872
Tia Willis <tiawillis1@icloud.com>
2022-12-07 13:11:09 -0700

    patrial wave 2 completed

--- src/index.js
+++ src/index.js
@@ -2,3 +2,44 @@
 'use strict';
 
 axios;
+
+var clicks = 32;
+
+function changeColor(el, el2) {
+  let color = 'black';
+  if (clicks <= 49) {
+    color = 'teal';
+  } else if (clicks <= 59) {
+    color = 'green';
+  } else if (clicks <= 69) {
+    color = 'yellow';
+  } else if (clicks <= 79) {
+    color = 'orange';
+  } else if (clicks > 79) {
+    color = 'red';
+  }
+  el.style.color = color;
+  el2.style.color = color;
+}
+
+function clicksUpTemp() {
+  clicks += 1;
+  document.getElementById('tempNumber').innerHTML = clicks;
+  changeColor(
+    document.getElementById('tempNumber'),
+    document.getElementById('tempHeader')
+  ); //connecting colorchanging Temp header and Temp Number to clicks
+}
+
+function clicksDownTemp() {
+  clicks -= 1;
+  document.getElementById('tempNumber').innerHTML = clicks;
+  changeColor(
+    document.getElementById('tempNumber'),
+    document.getElementById('tempHeader')
+  );
+}
+
+// function changeLandscape(el) {
+//   let landscape = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+// }
