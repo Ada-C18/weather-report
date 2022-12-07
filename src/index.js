795bf8157b745cb4d37bad53345cbaa4650a3654
Mia Yang <miacxyang@gmail.com>
2022-12-07 15:08:25 -0800

    Create Temp Buttons Functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 'use strict';
+const state = {
+    increaseTemp: null,
+    currentTemp: 60
+};
+
+const loadsControls = () => {
+    state.increaseTemp = document.getElementById("increaseTemp");
+};
+const increaseTempNum = () => {
+    state.increaseTemp.addEventListener("click", (event) => {
+        ++state.currentTemp;
+        state.currentTemp.textContent = state.currentTemp;
+    })
+};
