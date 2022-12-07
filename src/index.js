1d5d04d67d83e067932d4652694777edec3f0ebc
Soleil <soleilherring@gmail.com>
2022-12-07 14:13:04 -0800

    added button to increase and decrease temperature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-"use strict";
+'use strict';
 // const { default: axios } = require("axios")
-axios
+axios;
 
+{
+  /* <script src="./node_modules/axios/dist/axios.min.js"></script> */
+}
 
+// create state
+const state = {
+  temperature: 72,
+};
 
-{/* <script src="./node_modules/axios/dist/axios.min.js"></script> */}
+// create functions
+const increaseTemp = () => {
+  state.temperature++;
+  temperature.textContent = `${state.temperature}°`;
+  colorChange();
+};
+
+const decreaseTemp = () => {
+  state.temperature--;
+  temperature.textContent = `${state.temperature}°`;
+};
+
+// register event handlers
+const registerEventHandlers = () => {
+  // Increase Decrease Arrows
+  const arrowUp = document.getElementById('arrow-up');
+  arrowUp.addEventListener('click', increaseTemp);
+
+  const arrowDown = document.getElementById('arrow-down');
+  arrowDown.addEventListener('click', decreaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+// temperature color changes based on temp
+const colorChange = () => {
+  if (temperature.textContent >= 80) {
+    temperature.textContent.style.color = 'red';
+  }
+};
