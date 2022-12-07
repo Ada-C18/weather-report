046f042f16e0fa93e56cf7db63fcd3201cd723e7
JuliaPark <pje106@gmail.com>
2022-12-07 14:36:58 -0800

    add Event listener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 "use strict";
 
+// const state = {
+//     weatherCount: 0,
+//   };
+
+//   state.weatherCount += 1;
+//   const weatherCountContainer = document.querySelector("#crabCount")
+//   weatherCountContainer.textContent = `Crab Count: ${state.weatherCount}`;
+// ;
+
+// button.addEventListener("click", function() {
+//     var increment = document.getElementById("count");
+//     increment.innerText = count;
+//     count++;
+//   });
 // const registerEventHandlers = () => {
 //     const increaseButton = document.querySelector("#increaseButton");
 //     increaseButton.addEventListener("click", addCrab);
@@ -7,10 +21,21 @@
 
 //     ocument.addEventListener("DOMContentLoaded", registerEventHandlers);
 
-function incrementValue()
-{
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
-}
+// Select increment and decrement buttons
+
+// let displayTemperature = parseInt(document.querySelector('.displayTemperature'));
+
+
+let number = parseInt(document.querySelector('.number').innerHTML);
+const plus = document.querySelector('.plus_minus'),
+  minus = document.querySelector('.minus_plus');
+  
+plus.addEventListener('click',() => {
+    number++ ;
+    document.querySelector('.number').innerHTML = number;
+   });
+
+minus.addEventListener('click',() => {
+    number--;
+    document.querySelector('.number').innerHTML = number;
+   });
