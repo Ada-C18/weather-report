50e6ec8576955c85fa3277fc242d81a5d05a02cd
Melley Gebretatios <gmelley4@gmail.com>
2022-12-07 11:18:36 -0500

    Tried to link all three in Wave1.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 'use strict';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../styles/index.css" rel="stylesheet" type="text/css">
+    <script src="./src/index.js"></script>
+    <script src="./node_modules/axios/dist/axios.min.js"></script>
+    <title>Add title here</title>
+</head>
+<body>
+
+</body>
+</html>
