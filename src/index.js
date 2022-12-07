115a6240bda7ae0504e6dca723bed187cf122edd
nina-tuyen tran <ninatuyentran@gmail.com>
2022-12-07 14:44:37 -0800

    Created a tempColor function in JS to change the background of our temperature display box depending on the value of the temperature

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 const state = {
-    temp: 69,
+    temp: 50,
 }
+
+const displayTemp = document.getElementById("display-temp");
+    displayTemp.textContent = `${state.temp}°  F`;
+
+
+
 
 const increaseTemp = () => {
     const displayTemp = document.getElementById("display-temp");
     state.temp += 1;
     displayTemp.textContent = `${state.temp}°  F`;
+    tempColor(state.temp);
 }
 
 const decreaseTemp = () => {
     const displayTemp = document.getElementById("display-temp");
     state.temp -= 1;
     displayTemp.textContent = `${state.temp}°  F`;
+    tempColor(state.temp);
+}
+
+const tempColor = (temp) => {
+    const tempBG = document.getElementById("temperature");
+    //depending on value of state.temp, set bgColor
+    if (temp >= 80) {
+        tempBG.style.backgroundColor = 'red';
+    } else if (70 <= temp && temp < 80) {
+        tempBG.style.backgroundColor = 'orange';
+    } else if (60 <= temp && temp < 70) {
+        tempBG.style.backgroundColor = 'yellow';
+    } else if (50 <= temp && temp < 60) {
+        tempBG.style.backgroundColor = 'green';
+    } else if (temp <= 49) {
+        tempBG.style.backgroundColor = 'teal';
+    }
+    
+    // switch(temp) {
+    //     case temp >= 80:
+    //         tempBG.style.backgroundColor = 'red';
+    //     case 70 <= temp < 80:
+    //         tempBG.style.backgroundColor = 'orange';
+    //     case 60 <= temp < 70:
+    //         tempBG.style.backgroundColor = 'yellow';
+    //     case 50 <= temp < 60:
+    //         tempBG.style.backgroundColor = 'green';
+    //     case temp <= 49: 
+    //         tempBG.style.backgroundColor = 'teal';
+        
+    // }
+
 }
 
 const registerEventHandlers = () => {
@@ -22,3 +61,4 @@ const registerEventHandlers = () => {
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
+document.addEventListener("DOMContentLoaded", tempColor(state.temp));
