37e488ad42e8244bddeee32fc7c44a0b8e93e26d
hang tran <hangmtran101@gmail.com>
2022-12-07 12:07:33 -0800

    added buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// const registerEventHandlers = () => {
+//     const increaseButton = document.querySelector("#increaseButton");
+//     increaseButton.addEventListener("click", addCrab);
+
+
+//     ocument.addEventListener("DOMContentLoaded", registerEventHandlers);
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
