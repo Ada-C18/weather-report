afa5d32b8fa77ff4bc87b6e8c1fddc12430114ef
Samantha Hunt <ariaandsam@gmail.com>
2022-12-07 14:01:03 -0800

    Wave 2 and 3 done

--- src/index.js
+++ src/index.js
@@ -7,59 +7,73 @@
 // Add an event listener ( onClick )for both buttons ( ^ increases by 1, v decreases by 1 )
 // Add an event listener that checks temp and assigns color accordingly
 
-// const temperature = document.getElementById("display-temp")
+let temperature = document.getElementById("display-temp")
 
 //Set default temp (will update with Weather API using proxy server)
-// window.onLoad = function() => {temperature.innerText = 50}
+window.onload = () => {temperature.innerHTML = 50}
 
 //Temp Buttons:
-// const tempUp = document.getElementById("temp-up")
-// const tempDown = document.getElementById("temp-down")
-// tempUp.addEventListener('click', () => {temperature.innerText += 1})
-// tempDown.addEventListener('click', () => {temperature.innerText -= 1 changeColorAndEmojis(temperature)})
+const tempUp = document.getElementById("temp-up")
+const tempDown = document.getElementById("temp-down")
+tempUp.addEventListener('click', () => {
+    let tempHTML = Number(temperature.innerHTML)
+    tempHTML += 1;
+    temperature.innerHTML = tempHTML;
+    changeColorAndEmojis(temperature)
+})
+tempDown.addEventListener('click', () => {
+    let tempHTML = Number(temperature.innerHTML)
+    tempHTML -= 1;
+    temperature.innerHTML = tempHTML;
+    changeColorAndEmojis(temperature)
+})
 
+const landscape = document.getElementById("ground-emoji")
+const changeColorAndEmojis = (temperature) => {
 
-//Color change (needs to be in a function): 
+    if (temperature.innerText >= 80) {
+        temperature.style.color = 'red'
+        temperature.style.backgroundColor = 'white'
+        landscape.innerText = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂'
+    }
+    else if (temperature.innerText >= 70 && temperature.innerText <= 79) {
+        temperature.style.color = 'orange'
+        temperature.style.backgroundColor = 'white'
+        landscape.innerText = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷'
+    }
+    else if (temperature.innerText >= 60 && temperature.innerText <= 69) {
+        temperature.style.color = 'yellow'
+        temperature.style.backgroundColor = 'white'
+        landscape.innerText = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃'
+    }
+    else if (temperature.innerText >= 50 && temperature.innerText <= 59) {
+        temperature.style.color = 'green'
+        temperature.style.backgroundColor = 'white'
+        landscape.innerText = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲'
+    }
+    else {
+        temperature.style.color = 'teal'
+        temperature.style.backgroundColor = 'white'
+        landscape.innerText = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲'
+    }
+    // switch(Number(temperature.innerHTML)){
+    //     case Number(temperature.innerHTML) >= 80:
+    //         temperature.style.color = 'red'
+    //         temperature.style.backgroundColor = 'red'
+    //         landscape.innerText = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂'
+    //         break;
+    //     case Number(temperature.innerHTML) >= 70:
+    //         temperature.style.color = 'orange' 
+    //         temperature.style.backgroundColor = 'orange'
+    //         landscape.innerText = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷'
+    //         break;
+    // }
+}
 
-// const landscape = document.getElementById("ground-emoji")
-// const changeColorAndEmojis = (temperature) => {
-// if (temperature.innerText >= 80) {
-    //temperature.style.color = 'red'
-    //temperature.style.backgroundColor = someColor
-    //landscape.innerText = 🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂
-//}
-// else if (temperature.innerText >= 70 && temperature.innerText <= 79) {
-    //temperature.style.color = orange 
-    //temperature.style.backgroundColor = someColor
-    //landscape.innerText = 🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷
-//}
-// else if (temperature.innerText >= 60 && temperature.innerText <= 69) {
-    //temperature.style.color = yellow
-    //temperature.style.backgroundColor = someColor
-    //landscape.innerText = 🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃
-//}
-// else if (temperature.innerText >= 50 && temperature.innerText <= 59) {
-    //temperature.style.color = green
-    //temperature.style.backgroundColor = someColor
-    //landscape.innerText = 🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲
-//}
-// else {
-    //temperature.style.color = teal
-    //temperature.style.backgroundColor = someColor
-    //landscape.innerText = 🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲
-//}
+let inputCity = document.getElementById("input-city")
 
-// Potential switch case for color change! :
-// switch(temperature.innerText){
-// case >=80: 
-    //REPEAT FOR CASES:
-    //temperature.style.color = red 
-    //temperature.style.backgroundColor = someColor
-    //landscape.innerText = 🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂
-    //break
-// case >= 70: temperature.style.color = orange break
-// case >=60: temperature.style.color = yellow break
-// case >=50: temperature.style.color = green break
-// default: temperature.style.color = teal break
-// }
-
+let cityName = document.getElementById("city-name")
+const changeCity = (inputCity) => {
+    cityName.innerHTML = inputCity.value
+}
+inputCity.addEventListener('change', () => {changeCity(inputCity)})
