bf1a571e75d59e37d44bc89d6a4a5f36e938c2b6
masinakh <nakhjirimasi@gmail.com>
2022-12-06 21:19:41 -0800

    add color fot tempvalue

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 'use strict';
-
 const state = {
   temp: 80,
+  tempColor: 'red',
 };
-
-// const tempValue = document.getElementById('tempValue');
+// const tempValue = document.getElementById(‘tempValue’);
 // tempValue.textContent = state.temp;
-
 // const increaseTemp = () => {
 //   state.temp++;
 //   console.log(state);
-//   const increaseValue = document.getElementById('tempValue');
+//   const increaseValue = document.getElementById(‘tempValue’);
 //   increaseValue.textContent = `${state.temp}`;
 // };
-
 // const decreaseTemp = () => {
 //   state.temp--;
 //   console.log(state);
-//   const decreaseValue = document.getElementById('tempValue');
+//   const decreaseValue = document.getElementById(‘tempValue’);
 //   decreaseValue.textContent = `${state.temp}`;
 // };
-
 ///refactor the code above
 const showTemperature = () => {
   const tempValue = document.getElementById('tempValue');
   tempValue.textContent = state.temp + '\u00B0F';
+  // tempValue.style.color = ‘red’;
+  console.log(tempValue);
+};
+
+const tempColor = () => {
+  const tempColorElement = document.getElementById('tempValue');
+  if (state.temp <= 49) {
+    tempColorElement.style.color = 'teal';
+  } else if (50 <= state.temp && state.temp <= 59) {
+    tempColorElement.style.color = 'green';
+  } else if (60 <= state.temp && state.temp <= 69) {
+    tempColorElement.style.color = 'yellow';
+  } else if (70 <= state.temp && state.temp <= 79) {
+    tempColorElement.style.color = 'orange';
+  } else {
+    tempColorElement.style.color = 'red';
+  }
 };
 
 const changeTemp = (e) => {
@@ -42,9 +55,10 @@ const changeTemp = (e) => {
 const allEventHandlers = () => {
   const increaseArrow = document.getElementById('tempUp');
   increaseArrow.addEventListener('click', changeTemp);
+  increaseArrow.addEventListener('click', tempColor);
 
   const decreaseArrow = document.getElementById('tempDown');
   decreaseArrow.addEventListener('click', changeTemp);
+  decreaseArrow.addEventListener('click', tempColor);
 };
-
 document.addEventListener('DOMContentLoaded', allEventHandlers);
