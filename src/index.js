471b86c1eac67e73d3ce27e09e87715fcbe0f327
Tia Willis <tiawillis1@icloud.com>
2022-12-07 15:59:09 -0700

    wave 3 complete, js cleaned up

--- src/index.js
+++ src/index.js
@@ -50,8 +50,6 @@ function changeLandscape() {
   document.getElementById('landscapeImage').innerHTML = landscape;
 }
 
-// const currentCity = document.getElementById('currentCity');
-// const userInput = document.getElementById('cityName');
 function changeCityName() {
   var x = document.getElementById('cityName').value;
   document.getElementById('currentCity').innerHTML = x;
