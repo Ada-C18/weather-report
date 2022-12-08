5ff0f45a73ab2a2f8fdf008c6591d54224e7b23e
Tara <talsaidi@ucdavis.edu>
2022-12-07 16:18:31 -0800

    wave 2: added text color change and background change dependent on temperature from state.tempCount global variable

--- src/index.js
+++ src/index.js
@@ -6,20 +6,54 @@ const state = {
   tempCount: 0,
 };
 
+const changeTempColor = () => {
+  let temp = state.tempCount;
+  if (temp <= 49) {
+    document.getElementById('tempCounter').style.color = 'teal';
+  } else if (temp <= 59) {
+    document.getElementById('tempCounter').style.color = 'green';
+  } else if (temp <= 69) {
+    document.getElementById('tempCounter').style.color = 'yellow';
+  } else if (temp <= 79) {
+    document.getElementById('tempCounter').style.color = 'orange';
+  } else {
+    document.getElementById('tempCounter').style.color = 'red';
+  }
+};
+
+const changeBackground = () => {
+  let temp = state.tempCount;
+  if (temp <= 59) {
+    document.body.style.backgroundImage = "url('./assets/59_or_below.jpeg')";
+  } else if (temp <= 69) {
+    document.body.style.backgroundImage = "url('./assets/69_or_below.jpeg')";
+  } else if (temp <= 79) {
+    document.body.style.backgroundImage = "url('./assets/79_or_below.jpeg')";
+  } else {
+    document.body.style.backgroundImage = "url('./assets/80_and_above.png')";
+  }
+};
+
 const addDegree = () => {
   state.tempCount += 1;
   const tempCounter = document.querySelector('#tempCounter');
   tempCounter.textContent = `${state.tempCount} \u00B0`;
+  changeTempColor();
+  changeBackground();
+  console.log(state.tempCount);
 };
 const removeDegree = () => {
   state.tempCount -= 1;
   const tempCounter = document.querySelector('#tempCounter');
   tempCounter.textContent = `${state.tempCount} \u00B0`;
+  changeTempColor();
+  changeBackground();
 };
 
 const registerEventHandlers = () => {
   const increaseTempButton = document.querySelector('#increaseTempButton');
   increaseTempButton.addEventListener('click', addDegree);
+  // increaseTempButton.addEventListener('click', changeBackground);
 
   const decreaseTempButton = document.querySelector('#decreaseTempButton');
   decreaseTempButton.addEventListener('click', removeDegree);
