ded72350cd6c52e922e9901f387d240e431130a1
Marie Keefer <mkeefer17@gmail.com>
2022-12-08 14:01:02 -0500

    adds city name functionality and env file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 const state = {
-    
+    city: 'Seattle',
+    lat: 47.6038321,
+    long: -122.330062,
+    temp: 0
+}
+
+const convertKtoF = (temp) => {
+    return (temp - 273.15) * (9 / 5) + 32
+}
+
+const findLatAndLong = () => {
+    axios
+        .get()
 }
 
 const updateCityName = () => {
-    const inputName = document.getElementById('cityNameInput').value;
+    const inputName = document.getElementById('inputCityName').value;
     const headerCity = document.getElementById('headerCity');
     state.city = inputName;
     headerCity.textContent = state.city;
 };
-
-updateCityName();
-const cityNameInput = document.getElementById('cityNameInput');
-cityNameInput.addEventListener('input', updateCityName);
 
 let i = 0;
 let currentTemp = document.getElementById("tempValue");
@@ -32,6 +40,9 @@ const registerEventHandlers = () => {
     addTemp.addEventListener("click", increaseTemp);
     const lowerTemp = document.getElementById("decreaseTemp");
     lowerTemp.addEventListener("click", decreaseTemp);
+    updateCityName();
+    const cityNameInput = document.getElementById('cityNameInput');
+    cityNameInput.addEventListener('input', updateCityName);
 };
 
 
