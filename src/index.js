f691daa9b6c47133beaca53a03d9c5cf17fc1784
Isabella Van Stedum <isabella.vanstedum@gmail.com>
2022-12-08 12:54:48 -0600

    created functionality for updating city from input and header

--- src/index.js
+++ src/index.js
@@ -1,53 +1,60 @@
 const state = {
-  currentTemp: 80
-}
+  currentTemp: 80,
+  city: 'Seattle',
+};
 const increaseTemp = () => {
-  state.currentTemp += 1
+  state.currentTemp += 1;
 
-  const tempValue = document.getElementById("tempValue")
+  const tempValue = document.getElementById('tempValue');
 
-  tempValue.textContent = state.currentTemp
-  temperatureColorCheck(state.currentTemp)  
-}
-
-const decreaseTemp = () => {
-  state.currentTemp -= 1
-
-  const tempValue = document.getElementById("tempValue")
-
-  tempValue.textContent = state.currentTemp  
-  temperatureColorCheck(state.currentTemp)  
-
-}
-
-const temperatureColorCheck = (temp) => {
-  const tempValue = document.getElementById("tempValue")
-  const landscapePicture = document.getElementById("landscapePicture")
-
-  if (temp >= 80) {
-    tempValue.className = "red"
-    landscapePicture.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
-  } else if (temp >= 70) {
-    tempValue.className = "orange"
-    landscapePicture.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
-  } else if (temp >= 60) {
-    tempValue.className = "yellow"
-    landscapePicture.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
-  } else if (temp >= 50) {
-    tempValue.className = "green"
-    landscapePicture.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-  } else {
-    tempValue.className = "teal"
-  }
-}
-
-
-const registerEventHandlers = () => {
-  const increaseTempButton = document.getElementById("increaseTempButton");
-  increaseTempButton.addEventListener("click", increaseTemp);
-
-  const decreaseTempButton = document.getElementById("decreaseTempButton")
-  decreaseTempButton.addEventListener("click", decreaseTemp)
+  tempValue.textContent = state.currentTemp;
+  temperatureColorCheck(state.currentTemp);
 };
 
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+const decreaseTemp = () => {
+  state.currentTemp -= 1;
+
+  const tempValue = document.getElementById('tempValue');
+
+  tempValue.textContent = state.currentTemp;
+  temperatureColorCheck(state.currentTemp);
+};
+
+const temperatureColorCheck = (temp) => {
+  const tempValue = document.getElementById('tempValue');
+  const landscapePicture = document.getElementById('landscapePicture');
+
+  if (temp >= 80) {
+    tempValue.className = 'red';
+    landscapePicture.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (temp >= 70) {
+    tempValue.className = 'orange';
+    landscapePicture.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (temp >= 60) {
+    tempValue.className = 'yellow';
+    landscapePicture.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else if (temp >= 50) {
+    tempValue.className = 'green';
+    landscapePicture.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  } else {
+    tempValue.className = 'teal';
+  }
+};
+const updateCity = () => {
+  const inputCity = document.getElementById('input-city').value;
+  const cityName = document.getElementById('city-name');
+  state.city = inputCity;
+  cityName.textContent = state.city;
+};
+const registerEventHandlers = () => {
+  const increaseTempButton = document.getElementById('increaseTempButton');
+  increaseTempButton.addEventListener('click', increaseTemp);
+
+  const decreaseTempButton = document.getElementById('decreaseTempButton');
+  decreaseTempButton.addEventListener('click', decreaseTemp);
+
+  const inputCityBox = document.getElementById('input-city');
+  inputCityBox.addEventListener('input', updateCity);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
