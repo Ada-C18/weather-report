5536bec526c92f5ab4e7172831d9aa2a7b0ca08c
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-12-07 16:19:49 -0800

    add landscape

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ let counterDisplayElem = document.getElementById('temp-count');
 let counterMinusElem = document.querySelector('.counter-minus');
 let counterPlusElem = document.querySelector('.counter-plus');
 
+let landscapeElem = document.getElementById('landscape');
+
 // Temperature Helper Function
-const changeColor = (obj) => {
-  if (obj.innerHTML < 50) {
-    obj.className = 'teal';
-  } else if (obj.innerHTML < 60) {
-    obj.className = 'green';
-  } else if (obj.innerHTML > 70) {
-    obj.className = 'yellow';
-  } else if (obj.innerHTML < 80) {
-    obj.className = 'orange';
-  } else if (obj.innerHTML > 80) {
-    obj.className = 'red';
+const changeColor = (obj1, obj2) => {
+  if (obj1.innerHTML < 50) {
+    obj1.className = 'teal';
+    obj2.innerHTML = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  } else if (obj1.innerHTML < 60) {
+    obj1.className = 'green';
+    obj2.innerHTML = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂';
+  } else if (obj1.innerHTML > 70) {
+    obj1.className = 'yellow';
+    obj2.innerHTML = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else if (obj1.innerHTML < 80) {
+    obj1.className = 'orange';
+    obj2.innerHTML = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (obj1.innerHTML > 80) {
+    obj1.className = 'red';
+    obj2.innerHTML = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
   }
 };
 
@@ -22,15 +29,15 @@ const changeColor = (obj) => {
 const inputSubmitButton = document.getElementById('submit-button');
 
 const registerEventHandlers = () => {
-  //Temperature 
+  //Temperature
   counterPlusElem.addEventListener('click', () => {
     counterDisplayElem.innerHTML++;
-    changeColor(counterDisplayElem);
+    changeColor(counterDisplayElem, landscapeElem);
   });
 
   counterMinusElem.addEventListener('click', () => {
     counterDisplayElem.innerHTML--;
-    changeColor(counterDisplayElem);
+    changeColor(counterDisplayElem, landscapeElem);
   });
 
   // City Submit Bar
