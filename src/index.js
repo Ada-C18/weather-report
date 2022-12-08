f9d6365a9ca73d9234b137f9eee60693f5009fc8
Rahel Kemal <rahelskemal@gmail.com>
2022-12-07 21:03:04 -0800

    added temp incrementing event

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 const state = {
     
 }
@@ -14,3 +12,27 @@ const updateCityName = () => {
 updateCityName();
 const cityNameInput = document.getElementById('cityNameInput');
 cityNameInput.addEventListener('input', updateCityName);
+
+let i = 0;
+let currentTemp = document.getElementById("tempValue");
+currentTemp.innerHTML = i;
+
+const increaseTemp = () => {
+    i++;
+    currentTemp.innerHTML = i;
+};
+
+const decreaseTemp = () => {
+    i--;
+    currentTemp.innerHTML = i;
+};
+
+const registerEventHandlers = () => {
+    const addTemp = document.getElementById("increaseTemp");
+    addTemp.addEventListener("click", increaseTemp);
+    const lowerTemp = document.getElementById("decreaseTemp");
+    lowerTemp.addEventListener("click", decreaseTemp);
+};
+
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
