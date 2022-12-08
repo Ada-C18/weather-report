a5d355afee605f5734b1260730531a0cd0d96d4e
LexElena22 <103533828+LexElena22@users.noreply.github.com>
2022-12-07 17:48:19 -0800

    completed wave 03 city submit bar

--- src/index.js
+++ src/index.js
@@ -13,20 +13,20 @@ const changeColor = (obj1, obj2) => {
   } else if (obj1.innerHTML < 60) {
     obj1.className = 'green';
     obj2.innerHTML = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂';
-  } else if (obj1.innerHTML > 70) {
+  } else if (obj1.innerHTML < 70) {
     obj1.className = 'yellow';
     obj2.innerHTML = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
   } else if (obj1.innerHTML < 80) {
     obj1.className = 'orange';
     obj2.innerHTML = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
-  } else if (obj1.innerHTML > 80) {
+  } else if (obj1.innerHTML >= 80) {
     obj1.className = 'red';
     obj2.innerHTML = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
   }
 };
 
 // City Submit Bar Variables
-const inputSubmitButton = document.getElementById('submit-button');
+let inputSubmitButton = document.getElementById('submit-button');
 
 const registerEventHandlers = () => {
   //Temperature
@@ -42,8 +42,8 @@ const registerEventHandlers = () => {
 
   // City Submit Bar
   inputSubmitButton.addEventListener('click', () => {
-    console.log(document.getElementById('submit-city'));
-    console.log('you clicked submit');
+    let submitValue = document.getElementById('submit-city').value;
+    document.getElementById('city-name').textContent = submitValue;
   });
 };
 
