2218b022431b4f610248af7108e58f44b49935a9
hang tran <hangmtran101@gmail.com>
2022-12-07 22:08:30 -0800

    change color based on temp and added city element change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,56 @@
 "use strict";
 
-// const registerEventHandlers = () => {
-//     const increaseButton = document.querySelector("#increaseButton");
-//     increaseButton.addEventListener("click", addCrab);
+
+// make a color temp function
+// const colorChangeByTemp =function(number) {
 
 
-//     ocument.addEventListener("DOMContentLoaded", registerEventHandlers);
+// } return color
 
-function incrementValue()
-{
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
+let number = parseInt(document.querySelector('.number').innerHTML);
+const plus = document.querySelector('.plus_minus'),
+  minus = document.querySelector('.minus_plus');
+  
+plus.addEventListener('click',() => {
+    number++ ;
+    document.querySelector('.number').innerHTML = number;
+    if(number >= 80){
+        document.querySelector('.number').classList.add("burning")
+    }else if(number >=70) {
+        document.querySelector('.number').classList.add("hot")
+    } else if(number>=60) {
+        document.querySelector('.number').classList.add("moderate")
+    } else if(number >=50) {
+        document.querySelector('.number').classList.add("cold")
+    } else {
+        document.querySelector('.number').classList.add("freezing")
+
+   }});
+
+
+minus.addEventListener('click',() => {
+    number--;
+    document.querySelector('.number').innerHTML = number;
+    if(number >= 80) {
+        document.querySelector('.number').classList.add("burning")
+    } else if(number >=70) {
+        document.querySelector('.number').classList.remove("burning")
+    } else if(number>=60) {
+        document.querySelector('.number').classList.remove("hot");
+    } else if(number >=50) {
+        document.querySelector('.number').classList.remove("moderate")
+    }else {
+        document.querySelector('.number').classList.remove("cold")
+   }});
+
+
+let cityNameReset = document.getElementById("reset")
+cityNameReset.addEventListener("click", myFunction)
+
+
+function myFunction() {
+    let cityName = document.getElementById("cityName"); 
+    document.getElementById("intro").innerHTML = cityName.value;
 }
+
+document.getElementById("intro").innerHTML = "☁️"
