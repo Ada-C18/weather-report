037538a77d52dec548d16e24813941f2e0bca60b
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-12-08 10:01:58 -0800

    Created up and down arrows (not yet clickable) and added landscape photos to asseets

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// WAVE 2
+
+const increaseTemp = () => {
+  // set getting curr-temp item as a variable
+  // Create event listener that says upon a click, increase curr-temp by one
+};
+
+const decreaseTemp = () => {
+  // set getting curr-temp item as a variable???
+  // Create event listener that says upon a click, decrease curr-temp by one
+};
+
+const tempcolor = () => {
+  // create event handler that watches the curr-temp and changes the color accordingly
+  // 80+ is red (desert landscape)
+  // 70-79 is orange (tropical landscape)
+  // 55-69 is yellow (green field landscape)
+  // 40-54 is green (fall landscape)
+  // 39 and below is teal (winter landscape)
+};
