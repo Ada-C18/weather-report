660dfc498b79f7a218cbb84060a0edb5e08af6c7
Siyi Ji <monkeyqueenstacy@gmail.com>
2022-12-08 12:58:34 -0500

    fix html and wave 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function increaseTemp() {
+  console.log('ok');
+  const tempNumber = document.getElementById('temp-number');
+  console.log(tempNumber);
+  state.tempNumber.innerHTML += 1;
+}
+
+function updateCityName() {
+  const text = document.getElementById('city-name-input').value;
+  const header = document.getElementById('city-name');
+  header.innerHTML = text;
+}
+
+function resetCityName() {
+  const header = document.getElementById('city-name');
+  header.innerHTML = 'Austin';
+  document.getElementById('city-name-input').value = 'Austin';
+}
