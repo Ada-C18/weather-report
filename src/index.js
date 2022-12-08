6d72ad30820b0a551da8fdf940fded3f2d53e22d
Erika Torres Figueroa <torrespurple@gmail.com>
2022-12-08 13:55:52 -0500

    wave 2. temp color

--- src/index.js
+++ src/index.js
@@ -4,29 +4,45 @@ const state = {
   temp: 30,
 };
 
-const formatTempAndGarden = () => {
+const formatTempAndLandscape = () => {
   let temp = state.temp;
+  let color = 'fuschia';
+  // const p = document.getElementById('#tempDisplay');
+  if (temp > 80) {
+    color = 'red';
+  } else if (temp < 70) {
+    color = 'orange';
+  } else if (temp < 60) {
+    color = 'yellow';
+  } else if (temp < 50) {
+    color = 'green';
+  } else {
+    color = 'teal';
+  }
+
   const temperature = document.getElementById('tempDisplay');
+  temperature.className = color;
   temperature.textContent = String(state.temp);
 };
 
 const addTemp = () => {
   state.temp += 1;
-  formatTempAndGarden();
+  formatTempAndLandscape();
   const tempDisplay = document.querySelector('#tempDisplay');
   tempDisplay.textContent = `${state.temp}`;
 };
 
 const minusTemp = () => {
   state.temp -= 1;
-  formatTempAndGarden();
+  formatTempAndLandscape();
 };
 
 const registerEventHandlers = () => {
-  formatTempAndGarden();
+  formatTempAndLandscape();
 
   const tempPlus = document.getElementById('tempPlus');
   tempPlus.addEventListener('click', addTemp);
+  console.log(tempPlus);
 
   const tempMinus = document.getElementById('tempMinus');
   tempMinus.addEventListener('click', minusTemp);
