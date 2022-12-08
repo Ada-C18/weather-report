0d190da0435eeda2aed895d60b9d6c6728436cac
Tami <tamiadetayo@gmail.com>
2022-12-08 13:28:51 -0800

    Finished wave 2

--- src/index.js
+++ src/index.js
@@ -1,35 +1,56 @@
 'use strict';
 
 const state = {
-  temp: 0
+  temp: 65
 };
 
-const currentTemp = document.querySelector("#temp_value");
+const changeTempColorAndGardenLandscape = () => {
+  let temp = state.temp;
+  let color = "yellow";
+  let landscape = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+  if (temp > 80) {
+    color = "red";
+    landscape = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (temp > 70) {
+    color = "orange";
+    landscape = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (temp > 60) {
+    color = "yellow";
+    landscape = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+  } else if (temp > 50) {
+    color = "green";
+    landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+  } else {
+    color = "teal";
+    landscape = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+  }
+
+  const currentTemp = document.querySelector("#temp_value");
+  currentTemp.textContent = state.temp
+  currentTemp.className = color;
+  const currentLandscape = document.querySelector("#landscape");
+  currentLandscape.textContent = landscape;
+};
+
 
 const increaseTemp = () => {
   state.temp++;
-  currentTemp.textContent = String(state.temp);
+  changeTempColorAndGardenLandscape();
 };
 
 const decreaseTemp = () => {
-  const currentTemp = document.querySelector('#temp_value');
-  currentTemp--;
+  state.temp--;
+  changeTempColorAndGardenLandscape();
 }
 
 const registerEventHandlers = (event) => {
+  changeTempColorAndGardenLandscape();
+
   const increaseButton = document.querySelector("#increase_temp");
   increaseButton.addEventListener('click', increaseTemp);
+
   const decreaseButton = document.querySelector('#decrease_temp');
   decreaseButton.addEventListener('click', decreaseTemp);
 }
 
-const setup = () => {
-  increaseTemp();
-  decreaseTemp();
-}
-
-if (document.readyState !== "loading") {
-  setup();
-} else {
-  document.addEventListener("DOMContentLoaded", setup);
-}
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
