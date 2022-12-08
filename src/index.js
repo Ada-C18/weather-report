aebdf326afa5c6308cecc98106b5a0bbf6b1a436
Valentina Ruiz <v.zruiz06@gmail.com>
2022-12-08 09:59:07 -0800

    temp changes color on event click

--- src/index.js
+++ src/index.js
@@ -7,33 +7,32 @@
 const tempValue = document.getElementById("tempValue");
 // console.log(tempValue);
 
-// const tempRange = num => {
-//     num = tempValue
-//     console.log(num);
-//     if (num < 49) {
-//         return 'teal';
-//     } else if (num < 59) {
-//         return 'green';
-//     } else if (num < 69) {
-//         console.log('yellow');
-//         return 'yellow';
-//     } else if (num < 79) {
-//         return 'orange';
-//     } else {
-//         return 'red';
-//     }
-// }
+const tempRange = num => {
+    // num = tempValue
+    // console.log(num);
+    if (num < 49) {
+        return 'teal';
+    } else if (num < 59) {
+        return 'green';
+    } else if (num < 69) {
+        // console.log('yellow');
+        return 'yellow';
+    } else if (num < 79) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
 // console.log(tempRange);
 
 const upButton = document.getElementById('increaseTemp');
 upButton.addEventListener('click', e => {
     tempValue.innerHTML = Number(tempValue.innerHTML) + 1;
-    // tempValue.style.color = tempRange
-    // console.log(e);
+    tempValue.style.color = tempRange(Number(tempValue.innerHTML));
 });
 
 const downButton = document.getElementById('decreaseTemp');
 downButton.addEventListener('click', e => {
     tempValue.innerHTML = Number(tempValue.innerHTML) - 1;
-
+    tempValue.style.color = tempRange(Number(tempValue.innerHTML));
 });
