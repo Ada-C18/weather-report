ea84927b492c554c719533382ee54b2dea0b4ab3
fataneh <fkarandish1@gmail.com>
2022-12-08 14:09:22 -0800

    add dropdown menu

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ console.log('Hello, World Fataneh!');
 
 const axios = require('axios');
 axios
-  .get('API url')
+  .get('http://127.0.0.1:5000/location',
+  {
+    params:{
+      q:'seattle',
+      format:'json'
+    },
+  })
+
   .then((response) => {
     console.log('success!');
   })
