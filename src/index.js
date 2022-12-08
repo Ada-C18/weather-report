23ffbdd6ebbb1b3e0b7a196e4b9c2a18b4569fe2
Luciane Dias <lucilewa@yahoo.com>
2022-12-08 10:11:01 -0800

    finished wave 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
 'use strict';
+const state = {
+  temp: 65,
+  city: 'Seattle',
+  lat: 0,
+  lon: 0,
+};
+
+const tempColor = () => {
+  const currentTemp = document.getElementById('temp-display');
+  if (state.temp >= 80) {
+    currentTemp.style.backgroundColor = 'red';
+  } else if (state.temp >= 70) {
+    currentTemp.style.backgroundColor = 'orange';
+  } else if (state.temp >= 60) {
+    currentTemp.style.backgroundColor = 'yellow';
+  } else if (state.temp >= 50) {
+    currentTemp.style.backgroundColor = 'green';
+  } else {
+    currentTemp.style.backgroundColor = 'teal';
+  }
+};
+
+const landscapeImage = () => {
+  const landscape = document.getElementById('landscape');
+  let pic;
+  if (state.temp >= 80) {
+    pic = '__😍🐍_🦂_🌵🌵🌵__🐍_🏜_🦂'; //**** add picture ***
+  } else if (state.temp >= 70) {
+    pic = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷'; //**** add picture ***
+  } else if (state.temp >= 60) {
+    pic = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃'; //**** add picture ***
+  } else {
+    pic = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲'; //**** add picture ***
+  }
+  landscape.innerText = pic;
+};
+
+const updateTheme = () => {
+  const currentTemp = document.getElementById('temp-display');
+  currentTemp.textContent = `${state.temp}`;
+  tempColor();
+  landscapeImage();
+};
+
+const increaseTemp = () => {
+  state.temp += 1;
+  updateTheme();
+};
+
+const decreaseTemp = () => {
+  state.temp -= 1;
+  updateTheme();
+};
+
+const toFahrenheit = (temp) => {
+  return Math.floor(1.8 * (temp - 273) + 32);
+};
+
+const registerEventHandlers = () => {
+  const tempIncreaseButton = document.getElementById('increase');
+  tempIncreaseButton.addEventListener('click', increaseTemp);
+
+  const tempDecreaseButton = document.getElementById('decrease');
+  tempDecreaseButton.addEventListener('click', decreaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
