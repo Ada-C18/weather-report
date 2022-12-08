fcefb87828463741b74f662b26a0d9db533c7d6c
galinaramaniuk <galina.sergarz@gmail.com>
2022-12-07 19:22:48 -0500

    Added EventListener for increase/decrease temp buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// Select the HTML Element the event will occur on
+const increaseTempElement = document.getElementById('increaseTempBtn');
+const decreaseTempElement = document.getElementById('decreaseTempBtn');
+const currentTemp = document.getElementById('currentTemp');
+
+// Make a function to run when it occurs
+// const changeTemperature = () => {
+//   if (increaseTempElement) {
+//     currentTemp.textContent = parseInt(currentTemp.textContent) + 1;
+//   } else if (decreaseTempElement) {
+//     currentTemp.textContent = parseInt(currentTemp.textContent) - 1;
+//   }
+// };
+
+// Make a function to run when it occurs
+const increaseTemperature = (event) => {
+  currentTemp.textContent = parseInt(currentTemp.textContent) + 1;
+  console.dir(event);
+};
+
+const decreaseTemperature = () => {
+  currentTemp.textContent = parseInt(currentTemp.textContent) - 1;
+};
+
+// const changingColor = () => {
+//   if currentTemp > 80 // assign class
+// };
+
+// Register that function as an 'event listener'
+increaseTempElement.addEventListener('click', increaseTemperature);
+decreaseTempElement.addEventListener('click', decreaseTemperature);
