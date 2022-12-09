91619bd2136ed3fd8e0aaaf96af1eccbe3b6ae8d
galinaramaniuk <galina.sergarz@gmail.com>
2022-12-09 18:28:54 -0500

    Solved Wave 3. Created init func to initialize default city

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const DEFAULT_CITY = 'Seattle';
 // Select the HTML Element the event will occur on
 const increaseTempElement = document.getElementById('increaseTempBtn');
 const decreaseTempElement = document.getElementById('decreaseTempBtn');
 const currentTempTag = document.getElementById('currentTemp');
 const landscape = document.getElementById('landscape');
-const getCityName = document.getElementById('cityNameInput').value;
-const headerCityContainer = document.getElementById('headerCityName');
+const cityNameInput = document.getElementById('cityNameInput');
+const headerCityName = document.getElementById('headerCityName');
+const cityNameReset = document.getElementById('cityNameReset');
 
 const changingTempColorAndLandscape = () => {
   let currentTemp = parseInt(currentTempTag.textContent);
@@ -28,19 +30,6 @@ const changingTempColorAndLandscape = () => {
   }
 };
 
-const updateCity = () => {
-  // console.dir(headerCityContainer);
-  // console.dir(getCityName);
-  headerCityContainer.value = getCityName;
-  // console.dir(headerCityContainer);
-};
-
-const resetCity = () => {
-  const cityContainer = document.getElementById("cityNameInput");
-  cityContainer.textContent = "Seattle";
-  updateCity();
-};
-
 // Register that function as an 'event listener'
 // increaseTempElement.addEventListener('click', increaseTemperature);
 increaseTempElement.addEventListener('click', () => {
@@ -53,12 +42,22 @@ decreaseTempElement.addEventListener('click', () => {
   changingTempColorAndLandscape();
 });
 
-cityNameInput.addEventListener('change', () => {
-  updateCity();
+cityNameInput.addEventListener('input', (inputEvent) => {
+  headerCityName.textContent = inputEvent.target.value;
 });
 
+cityNameReset.addEventListener('click', () => {
+  cityNameInput.value = DEFAULT_CITY;
+  headerCityName.textContent = DEFAULT_CITY;
+});
+
+const init = () => {
+  headerCityName.textContent = DEFAULT_CITY;
+  cityNameInput.value = DEFAULT_CITY;
+};
+init();
 // Make a function to run when it occurs
-// const increaseTemperature = (event) => {
+// const increaseTemperature = () => {
 //   currentTemp.textContent = parseInt(currentTemp.textContent) + 1;
 // };
 // const decreaseTemperature = () => {
