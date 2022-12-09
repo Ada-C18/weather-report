3cca2793716c78bba46e1800bf0cf3fbbff1b6f7
Nicole Mejia <snicolemejia@gmail.com>
2022-12-09 16:13:35 -0500

    added visual to change with temp ranges

--- src/index.js
+++ src/index.js
@@ -19,17 +19,23 @@ const decreaseDreamTemp = () => {
 
 const updateDreamTempColor = () => {
   const dreamTemp = document.querySelector("#dream-temp-number");
+  const tempCircle = document.querySelector("#circle");
 
   if (state.temp >= 80) {
     dreamTemp.style.color = "#E0392D";
+    tempCircle.style.backgroundColor = "#E0392D";
   } else if (70 <= state.temp && state.temp <= 79) {
     dreamTemp.style.color = "#E0571D";
+    tempCircle.style.backgroundColor = "#E0571D";
   } else if (60 <= state.temp && state.temp <= 69) {
     dreamTemp.style.color = "#F09104";
+    tempCircle.style.backgroundColor = "#F09104";
   } else if (50 <= state.temp && state.temp <= 59) {
     dreamTemp.style.color = "blue";
+    tempCircle.style.backgroundColor = "blue";
   } else if (state.temp <= 49) {
     dreamTemp.style.color = "purple";
+    tempCircle.style.backgroundColor = "purple";
   }
 };
 
