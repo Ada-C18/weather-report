360fc6a4633c6973d29db5c8424772ff0e22c7fb
Yun <ly.yun811@gmail.com>
2022-12-08 19:57:50 -0800

    Need to reconsider making variables scope global

--- src/index.js
+++ src/index.js
@@ -1,148 +1,69 @@
-const urlLocation ='http://127.0.0.1:5000/location'
-const urlWeather = 'http://127.0.0.1:5000/weather'
-
+const urlLocation = 'http://127.0.0.1:5000/location';
+const urlWeather = 'http://127.0.0.1:5000/weather';
 
 const windowLoad = () => {
-    const tempUp = document.getElementById('temp-up');
-    const tempDown = document.getElementById('temp-down');
-    const getRealTemp = document.getElementById('realtime');
-    const resetButton = document.getElementById('reset-button')
+  const tempUp = document.getElementById('temp-up');
+  const tempDown = document.getElementById('temp-down');
+  const getRealTemp = document.getElementById('realtime');
+  const resetButton = document.getElementById('reset-button');
 
-    let temperature = document.getElementById('display-temp');
-    let cityName = document.getElementById('city-name');
-    let defaultCity = 'Seattle'
-    let selectSky = document.getElementById('sky');
-    let sky = document.getElementById('sky-emoji');
-    let inputCity = document.getElementById('input-city');
-    
-    cityName.innerHTML = defaultCity;
+  let temperature = document.getElementById('display-temp');
+  let cityName = document.getElementById('city-name');
+  let defaultCity = 'Seattle';
+  let selectSky = document.getElementById('sky');
+  let sky = document.getElementById('sky-emoji');
+  let inputCity = document.getElementById('input-city');
+  // Yun: With JS, if we define (let xx =) these variables here inside this function windowLoad,
+  // we cannot use them in other functions. Something we need to reconsider.
+  // On my browser, it's giving me an error saying "xx is not defined" on line 121, line 126,
+  // if they are not global variables.
 
-    tempUp.addEventListener('click', () => {
-        let tempHTML = Number(temperature.innerHTML);
-        tempHTML += 1;
-        temperature.innerHTML = tempHTML;
-        changeColorAndEmojis(temperature);
-    });
-        tempDown.addEventListener('click', () => {
-        let tempHTML = Number(temperature.innerHTML);
-        tempHTML -= 1;
-        temperature.innerHTML = tempHTML;
-        changeColorAndEmojis(temperature);
-    });
-    const changeCity = (inputCity) => {
-        cityName.innerHTML = inputCity.value;
-    };
-    inputCity.addEventListener('change', () => {
-        changeCity(inputCity);
-    });
-    selectSky.addEventListener('change', () => {
-        console.log(selectSky.options[selectSky.selectedIndex].value);
-        changeSky(selectSky.options[selectSky.selectedIndex].value);
-    });
-    const changeSky = (selectedSky) => {
-        if (selectedSky === 'sunny') {
-            sky.innerHTML = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
-        } else if (selectedSky === 'cloudy') {
-            sky.innerHTML = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
-        } else if (selectedSky === 'rainy') {
-            sky.innerHTML = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
-        } else if (selectedSky === 'snowy') {
-            sky.innerHTML = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
-        }
-    };
-      
-    resetButton.addEventListener('click', () => {
-        cityName.innerHTML = defaultCity;
-    });
-    getRealTemp.addEventListener('click', () => {
-        getRealtimeTemp(cityName, temperature);
-    });
-    
-}
+  cityName.innerHTML = defaultCity;
 
-const changeColorAndEmojis = (temperature) => {
-    const landscape = document.getElementById('ground-emoji');
-    if (temperature.innerText >= 80) {
-        temperature.style.color = 'red';
-        temperature.style.backgroundColor = 'white';
-        landscape.innerText = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
-    } else if (temperature.innerText >= 70 && temperature.innerText <= 79) {
-        temperature.style.color = 'orange';
-        temperature.style.backgroundColor = 'white';
-        landscape.innerText = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
-    } else if (temperature.innerText >= 60 && temperature.innerText <= 69) {
-        temperature.style.color = 'yellow';
-        temperature.style.backgroundColor = 'white';
-        landscape.innerText = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
-    } else if (temperature.innerText >= 50 && temperature.innerText <= 59) {
-        temperature.style.color = 'green';
-        temperature.style.backgroundColor = 'white';
-        landscape.innerText = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
-    } else {
-        temperature.style.color = 'teal';
-        temperature.style.backgroundColor = 'white';
-        landscape.innerText = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  tempUp.addEventListener('click', () => {
+    let tempHTML = Number(temperature.innerHTML);
+    tempHTML += 1;
+    temperature.innerHTML = tempHTML;
+    changeColorAndEmojis(temperature);
+  });
+  tempDown.addEventListener('click', () => {
+    let tempHTML = Number(temperature.innerHTML);
+    tempHTML -= 1;
+    temperature.innerHTML = tempHTML;
+    changeColorAndEmojis(temperature);
+  });
+  const changeCity = (inputCity) => {
+    cityName.innerHTML = inputCity.value;
+  };
+  inputCity.addEventListener('change', () => {
+    changeCity(inputCity);
+  });
+  selectSky.addEventListener('change', () => {
+    console.log(selectSky.options[selectSky.selectedIndex].value);
+    changeSky(selectSky.options[selectSky.selectedIndex].value);
+  });
+  const changeSky = (selectedSky) => {
+    if (selectedSky === 'sunny') {
+      sky.innerHTML = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
+    } else if (selectedSky === 'cloudy') {
+      sky.innerHTML = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
+    } else if (selectedSky === 'rainy') {
+      sky.innerHTML = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
+    } else if (selectedSky === 'snowy') {
+      sky.innerHTML = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
     }
+  };
+
+  resetButton.addEventListener('click', () => {
+    cityName.innerHTML = defaultCity;
+  });
+  getRealTemp.addEventListener('click', () => {
+    getRealtimeTemp(cityName, temperature);
+  });
 };
-
-
-const getRealtimeTemp = (cityName, temperature) => {
-    console.log("here")
-    axios.get(urlLocation, {
-        params: {
-            q: cityName.innerHTML
-        }
-    })
-    .then(response => {
-        let lat = response.data[0].lat
-        let lon = response.data[0].lon
-        console.log(lat, lon)
-        return axios.get(urlWeather, {
-            params: {
-                lat: lat,
-                lon: lon,
-                units: 'imperial'
-            }
-        })
-    })
-    .then(response => {
-        currentTemp = Number(response.data.main.temp)
-        temperature.innerHTML = Math.floor((currentTemp - 273.15)*1.8)+32
-        changeColorAndEmojis(temperature);
-    })
-    .catch(err => {
-        console.log(err)
-    })
-}
-
-
-window.onload = () => {
-  temperature.innerHTML = 50;
-};
-window.onload(cityName.innerHTML = defaultCity)
-window.onload(getRealtimeTemp(cityName))
-
-const landscape = document.getElementById('ground-emoji');
-const tempUp = document.getElementById('temp-up');
-const tempDown = document.getElementById('temp-down');
-
-let inputCity = document.getElementById('input-city');
-
-
-tempUp.addEventListener('click', () => {
-  let tempHTML = Number(temperature.innerHTML);
-  tempHTML += 1;
-  temperature.innerHTML = tempHTML;
-  changeColorAndEmojis(temperature);
-});
-tempDown.addEventListener('click', () => {
-  let tempHTML = Number(temperature.innerHTML);
-  tempHTML -= 1;
-  temperature.innerHTML = tempHTML;
-  changeColorAndEmojis(temperature);
-});
 
 const changeColorAndEmojis = (temperature) => {
+  const landscape = document.getElementById('ground-emoji');
   if (temperature.innerText >= 80) {
     temperature.style.color = 'red';
     temperature.style.backgroundColor = 'white';
@@ -166,6 +87,65 @@ const changeColorAndEmojis = (temperature) => {
   }
 };
 
+const getRealtimeTemp = (cityName, temperature) => {
+  console.log('here');
+  axios
+    .get(urlLocation, {
+      params: {
+        q: cityName.innerHTML,
+      },
+    })
+    .then((response) => {
+      let lat = response.data[0].lat;
+      let lon = response.data[0].lon;
+      console.log(lat, lon);
+      return axios.get(urlWeather, {
+        params: {
+          lat: lat,
+          lon: lon,
+          units: 'imperial',
+        },
+      });
+    })
+    .then((response) => {
+      currentTemp = Number(response.data.main.temp);
+      temperature.innerHTML = Math.floor((currentTemp - 273.15) * 1.8) + 32;
+      changeColorAndEmojis(temperature);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+window.onload = () => {
+  // let temperature = document.getElementById('display-temp');
+  temperature.innerHTML = 50;
+};
+
+// let cityName = document.getElementById('city-name');
+// let defaultCity = 'Seattle';
+window.onload((cityName.innerHTML = defaultCity));
+window.onload(getRealtimeTemp(cityName));
+
+const landscape = document.getElementById('ground-emoji');
+const tempUp = document.getElementById('temp-up');
+const tempDown = document.getElementById('temp-down');
+
+let inputCity = document.getElementById('input-city');
+
+tempUp.addEventListener('click', () => {
+  let tempHTML = Number(temperature.innerHTML);
+  tempHTML += 1;
+  temperature.innerHTML = tempHTML;
+  changeColorAndEmojis(temperature);
+});
+tempDown.addEventListener('click', () => {
+  let tempHTML = Number(temperature.innerHTML);
+  tempHTML -= 1;
+  temperature.innerHTML = tempHTML;
+  changeColorAndEmojis(temperature);
+});
+
 const changeCity = (inputCity) => {
   cityName.innerHTML = inputCity.value;
 };
@@ -175,34 +155,24 @@ inputCity.addEventListener('change', () => {
 
 let selectSky = document.getElementById('sky');
 let sky = document.getElementById('sky-emoji');
-// const changeSky = (selectedSky) => {
-//   if (selectedSky === 'Sunny') {
-//     sky.innerHTML = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
-//   } else if (selectSky.options[selectSky.selectedIndex].value === 'Cloudy') {
-//     sky.innerHTML = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
-//   } else if (selectSky.value === 'Rainy') {
-//     sky.innerHTML = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
-//   } else if (selectSky.value === 'Snowy') {
-//     sky.innerHTML = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
-//   }
-// };
+
 selectSky.addEventListener('change', () => {
   console.log(selectSky.options[selectSky.selectedIndex].value);
   changeSky(selectSky.options[selectSky.selectedIndex].value);
 });
 const changeSky = (selectedSky) => {
-    if (selectedSky === 'sunny') {
-      sky.innerHTML = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
-    } else if (selectedSky === 'cloudy') {
-      sky.innerHTML = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
-    } else if (selectedSky === 'rainy') {
-      sky.innerHTML = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
-    } else if (selectedSky === 'snowy') {
-      sky.innerHTML = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
-    }
-  };
+  if (selectedSky === 'sunny') {
+    sky.innerHTML = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
+  } else if (selectedSky === 'cloudy') {
+    sky.innerHTML = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
+  } else if (selectedSky === 'rainy') {
+    sky.innerHTML = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
+  } else if (selectedSky === 'snowy') {
+    sky.innerHTML = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
+  }
+};
 
-const resetButton = document.getElementById('reset-button')
+const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click', () => {
-    cityName.innerHTML = defaultCity
-})
+  cityName.innerHTML = defaultCity;
+});
