8ec552c88d8487357b3980b72305412e198e3db2
bukunmig <bgesinde44@icloud.com>
2022-12-08 16:46:06 -0800

    test

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const displayedTemp = document.getElementById("displayedTemp");
+const DEFAULT_TEMP = 72;
+
+const increaseTemp = document.getElementById("increaseTemp");
+const decreaseTemp = document.getElementById("decreaseTemp");
+
+addEventListener()
