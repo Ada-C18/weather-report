5205b955f9047587f48e5ef2116ff90c03d4be67
Elsje Slothower <elsje.slothower@gmail.com>
2022-12-08 17:04:14 -0800

    added loadControls() & tempChange()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-const displayedTemp = document.getElementById("displayedTemp");
 const DEFAULT_TEMP = 72;
+const tempState = {
+  displayedTemp: null,
+  increaseTemp: null,
+  decreaseTemp: null,
+}
 
-const increaseTemp = document.getElementById("increaseTemp");
-const decreaseTemp = document.getElementById("decreaseTemp");
 
 addEventListener()
+
+const loadControls = () => {
+  tempState.displayedTemp = document.getElementById("displayedTemp");
+  tempState.increaseTemp = document.getElementById("increaseTemp");
+  tempState.decreaseTemp = document.getElementById("decreaseTemp");
+}
+
+
+const tempChange() => {
+  tempState.decreaseTemp.addEventListener("click", (event) => {
+    const newTemp = tempState.displayedTemp += 1;
+    tempState.displayedTemp = newTemp;
+  });
+  console.log("clicked");
+}
