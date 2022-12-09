d0bfccfa4d389aaa6f06f95a03cb7a61deb069a8
Aria Stroud <stroudaria@gmail.com>
2022-12-09 15:23:59 -0800

    Adds function to change temperature color and landscape depending on the temperature. Adjusts css and html to correspond.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,51 @@
+const state = {
+  currentTemp: 70,
+};
+
 const tempUp = () => {
-  console.log('up');
-  const currentTemp = document.querySelector('#tempValue');
-  currentTemp.textContent = parseInt(currentTemp.innerHTML) + 1;
+  const currentTemp = document.querySelector('#temp-value');
+  state.currentTemp += 1;
+  currentTemp.textContent = state.currentTemp;
 };
 
 const tempDown = () => {
-  console.log('down');
-  const currentTemp = document.querySelector('#tempValue');
-  currentTemp.textContent = parseInt(currentTemp.innerHTML) - 1;
+  const currentTemp = document.querySelector('#temp-value');
+  state.currentTemp -= 1;
+  currentTemp.textContent = state.currentTemp;
+};
+
+const changeColorAndLandscape = () => {
+  if (state.currentTemp > 79) {
+    document.querySelector('#temp-value').style.color = 'red';
+    document.querySelector('#landscape').textContent =
+      '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (state.currentTemp > 69 && state.currentTemp < 80) {
+    document.querySelector('#temp-value').style.color = 'orange';
+    document.querySelector('#landscape').textContent =
+      '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (state.currentTemp > 59 && state.currentTemp < 70) {
+    document.querySelector('#temp-value').style.color = 'yellow';
+    document.querySelector('#landscape').textContent =
+      '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else if (state.currentTemp > 49 && state.currentTemp < 60) {
+    document.querySelector('#temp-value').style.color = 'green';
+    document.querySelector('#landscape').textContent =
+      '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  } else if (state.currentTemp < 50) {
+    document.querySelector('#temp-value').style.color = 'lightblue';
+    document.querySelector('#landscape').textContent =
+      '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  }
 };
 
 const registerEventHandlers = () => {
-  const increaseTemp = document.querySelector('#tempUp');
+  const increaseTemp = document.querySelector('#temp-up');
   increaseTemp.addEventListener('click', tempUp);
+  increaseTemp.addEventListener('click', changeColorAndLandscape);
 
-  const decreaseTemp = document.querySelector('#tempDown');
+  const decreaseTemp = document.querySelector('#temp-down');
   decreaseTemp.addEventListener('click', tempDown);
+  decreaseTemp.addEventListener('click', changeColorAndLandscape);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
