ea2af4f4cfd7372e086913a97f9a98d9ad9c6c99
Eve Stroud <evestroud89@gmail.com>
2022-12-09 10:12:48 -0500

    Make page load temp & unit from State object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const cityName = document.querySelector('#city-name');
-const temperature = document.querySelector('#temperature');
+const temperature = document.querySelector('#temp');
+const tempUnit = document.querySelector('#temp-unit');
 const weather = document.querySelector('#weather');
 const buttonIncrease = document.querySelector('#button-increase');
 const buttonDecrease = document.querySelector('#button-decrease');
+
+const State = {
+	temperature: 70,
+	unit: 'F',
+};
+
+const updateWeather = function() {
+	temperature.textContent = `${State.temperature}`;
+	tempUnit.textContent = `${State.unit}`;
+};
+
+updateWeather();
+
+buttonIncrease.addEventListener('click', (_) => { });
