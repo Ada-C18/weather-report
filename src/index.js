f30b408ba0ac8eea6228d13b3ec891031de7a7d2
Eve Stroud <evestroud89@gmail.com>
2022-12-09 10:45:20 -0500

    Add landscape change placeholder behavior

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const cityName = document.querySelector('#city-name');
 const temperature = document.querySelector('#temp');
 const tempUnit = document.querySelector('#temp-unit');
 const weather = document.querySelector('#weather');
+const sky = document.querySelector('#sky');
+const landscape = document.querySelector('#landscape');
 const buttonIncrease = document.querySelector('#button-increase');
 const buttonDecrease = document.querySelector('#button-decrease');
 
@@ -15,6 +17,16 @@ const State = {
 const updateWeather = function() {
 	temperature.textContent = `${State.temperature}`;
 	tempUnit.textContent = `${State.unit}`;
+
+	if (State.temperature < 32) {
+		landscape.className = 'landscape-cold';
+	} else if (State.temperature < 60) {
+		landscape.className = 'landscape-cool';
+	} else if (State.temperature < 90) {
+		landscape.className = 'landscape-warm';
+	} else {
+		landscape.className = 'landscape-hot';
+	}
 };
 
 updateWeather();
