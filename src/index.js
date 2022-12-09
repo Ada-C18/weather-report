cdc18db3384bad8c1621e523988ac21cfde8b0f4
Diana Soriano <dssori@gmail.com>
2022-12-09 12:34:32 -0500

    updated 4 boxes still need to float on 2 x 2 matrix

--- src/index.js
+++ src/index.js
@@ -6,25 +6,27 @@ const { default: axios } = require("axios");
 
 const state = {
   city: 'Seattle',
-  lat:
-  long: 
-  temp: 
+  lat: 47.6038321,
+  long: -122.3300624,
+  temp: 50
 };
 
 
 const findLatAndLong = () => {
 axios
-  .get('', 
-  params: {
-    q: state.lat
+  .get('',
+    params: {
+    key: process.env['LOCATION_API_KEY'],
+    q: state.lat,
+    format: 'json',
   })
   .then((response) => {
     console.log(response.data);
     state.lat = response.data[0].lat;
     state.long = response.data[0].lon;
-
-
+  }
 };
+
 const findWeather = () => {
   axios
     .get('https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={WEATHER_API_KEY}',{ 
@@ -34,25 +36,55 @@ const findWeather = () => {
     })
   };
 
+  const increaseTemp = () => {
+    state.temp += 1;
+    formatTempAndGarden();
+  };
 
 // Increase/Decrease temperature
-const increaseTemp = () => {
-  let count = 0;
-  count++;
-  const upButton = document.getElementById('up');
-  const downButton = document.getElementById('down');
-  let display = document.getElementById('tempNum');
+// const increaseTemp = () => {
+//   let count = 0;
+//   count++;
+//   const upButton = document.getElementById('up');
+//   const downButton = document.getElementById('down');
+//   let display = document.getElementById('tempNum');
 
-  upButton.addEventListener('click', function () {
-    count++;
-    display.textContent = count;
-  });
-  downButton.addEventListener('click', function () {
-    count--;
-    display.textContent = count;
-  });
-  // increaseTemp.addEventListener("click", incrementCount);
-};
+//   upButton.addEventListener('click', function () {
+//     count++;
+//     display.textContent = count;
+//   });
+//   downButton.addEventListener('click', function () {
+//     count--;
+//     display.textContent = count;
+//   });
+//   // increaseTemp.addEventListener("click", incrementCount);
+// };
+
+
+const updateSky = () => {
+  const inputSky = document.getElementById('skyOptions').value;
+  const skyContainer = document.getElementById('sky-weather');
+  let sky = '';
+  let skyColor = '';
+  if (inputSky === 'Clouds') {
+    sky = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
+    skyColor = 'cloud';
+  } else if (inputSky === 'Sunshine') {
+    sky = '☁️     ☁️   ☁️ ☀️ ☁️  ☁️';
+    skyColor = 'sunn';
+  } else if (inputSky === 'Rain') {
+    sky = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
+    skyColor = 'rain';
+  } else if (inputSky === 'Snow') {
+    sky = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
+    skyColor = 'snow';
+  } else if (inputSky === 'Wind') {
+    sky = '';
+    skyColor = 'wind';
+  }
+}
+
+
 
 // const decreaseTemp = () => {
 //   i--;
