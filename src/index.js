4b4086ffad1bdb3c2ae1fccf8907ad1c724e36e0
Lindsey <lindseymarcea@icloud.com>
2022-12-08 23:15:07 -0500

    Working on adding weather garden/landscape code

--- src/index.js
+++ src/index.js
@@ -8,39 +8,50 @@ const state = {
 
 const increaseTemperature = () => {
   state.temp += 1;
-  changeColor();
+  changeColorAndGarden();
   // const temperature = document.getElementById('temp');
   // temperature.textContent = state.temp;
 };
 
 const decreaseTemperature = () => {
   state.temp -= 1;
-  changeColor();
+  changeColorAndGarden();
   // const temperature = document.getElementById('temp');
   // temperature.textContent = state.temp;
 };
 
-const changeColor = () => {
+const changeColorAndGarden = () => {
   let temp = state.temp;
   let color = 'red';
+  // let garden = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+
   if (temp >= 80) {
     color = 'red';
+    garden = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
   } else if (temp >= 70) {
     color = 'orange';
+    garden = '🦜🦜__😎_ 🌞__⛱⛱_ ';
   } else if (temp >= 60) {
     color = 'yellow';
+    garden = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
   } else if (temp >= 50) {
     color = 'green';
+    garden = '🌱🌱__ 🌻 🌿__ 🌷🌷';
   } else {
     color = 'teal';
+    garden = '❄️🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲️';
   }
 
+  const newgarden = document.getElementById('garden');
+  newgarden.textContent = garden;
   const temperature = document.getElementById('temp');
   temperature.className = color;
   temperature.textContent = String(state.temp);
 };
 
 const registerEventHandlers = () => {
+  changeColorAndGarden();
+
   const increaseTemp = document.getElementById('increaseTemp');
   increaseTemp.addEventListener('click', increaseTemperature);
 
