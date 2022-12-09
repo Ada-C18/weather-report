7850921201d5434d6b2f6906bab31969917e4dc9
Siyi Ji <monkeyqueenstacy@gmail.com>
2022-12-09 12:30:14 -0500

    working on wave4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('Online')
+console.log('Online');
 
 function increaseTemp() {
   console.log('ok');
@@ -8,7 +8,7 @@ function increaseTemp() {
   let tempNumber = parseInt(tempDiv.textContent);
   console.log(tempNumber);
   tempNumber += 1;
-  tempDiv.innerHTML = tempNumber
+  tempDiv.innerHTML = tempNumber;
 }
 
 function decreaseTemp() {
@@ -17,7 +17,7 @@ function decreaseTemp() {
   let tempNumber = parseInt(tempDiv.textContent);
   console.log(tempNumber);
   tempNumber -= 1;
-  tempDiv.innerHTML = tempNumber
+  tempDiv.innerHTML = tempNumber;
 }
 
 function updateCityName() {
@@ -30,4 +30,64 @@ function resetCityName() {
   const header = document.getElementById('city-name');
   header.innerHTML = 'Austin';
   document.getElementById('city-name-input').value = 'Austin';
+}
+
+function getLocation() {
+  const cityName = document.getElementById('city-name-input').value;
+  let latitude, longitude;
+  axios
+    .get('http://127.0.0.1:5000/location', {
+      params: {
+        q: cityName,
+      },
+    })
+    .then((res) => {
+      latitude = res.data[0].lat;
+      longitude = res.data[0].lon;
+      console.log('success in getLocation!', latitude, longitude);
+    })
+    .catch((error) => {
+      console.log('error in getLocation!');
+    });
+  // console.log('lat ' + latitude + ' lon ' + longitude);
+  console.log(latitude, latitude);
+  return { lat: latitude, lon: longitude };
+}
+
+function getTemperature(latitude, longitude) {
+  axios
+    .get('http://127.0.0.1:5000/weather', {
+      params: {
+        lat: latitude,
+        lon: longitude,
+      },
+    })
+    .then((res) => {
+      console.log('success in getTemp!', res.data);
+      return res.data['current']['temp'];
+    });
+}
+
+function updateRealtimeTemp() {
+  const cityName = document.getElementById('city-name-input').value;
+  let latitude, longitude;
+  axios
+    .get('http://127.0.0.1:5000/location', {
+      params: {
+        q: cityName,
+      },
+    })
+    .then((res) => {
+      latitude = res.data[0].lat;
+      longitude = res.data[0].lon;
+      console.log('success in getLocation!', latitude, longitude);
+      let temp = getTemperature(latitude, longitude);
+    })
+    .catch((error) => {
+      console.log('error in getLocation!');
+    });
+  console.log(locationData);
+  // let latitude = locationData.lat;
+  // let longitude = locationData.lon;
+  console.log('lat ' + latitude + ' lon ' + longitude);
 }
