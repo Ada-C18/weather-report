64dc37b4587bf4d40efb642afaf7e18cb5fa8b7f
Aretta Brown <aretta.brown@gmail.com>
2022-12-08 22:04:15 -0500

    fixes my commits

--- src/index.js
+++ src/index.js
@@ -11,49 +11,35 @@ const state = {
 const temperature = document.getElementById('tempValue');
 
 const updateBackground = () => {
-  const backgroundImage = document.getElementById('bg').style.backgroundImage;
+  const background = document.getElementById('bg');
 
   if (state.tempValue >= 100){
-    backgroundImage = 'url(../../assets/sun-surface.jpg)'
+    background.style.backgroundImage = 'url(../../assets/sun-surface.jpg)'
   }
   else if (state.tempValue >= 90){
-    backgroundImage = 'url(../../assets/desert.jpg)'
+    background.style.backgroundImage = 'url(../../assets/desert.jpg)'
   }
   else if (state.tempValue >= 80){
-    backgroundImage = 'url(../../assets/summer2.jpg)'
+    background.style.backgroundImage = 'url(../../assets/summer2.jpg)'
   }
   else if (state.tempValue >= 70){
-    backgroundImage = 'url(../../assets/summer.jpg)'
+    background.style.backgroundImage = 'url(../../assets/summer.jpg)'
   }
   else if (state.tempValue >= 60){
-    backgroundImage = 'url(../../assets/spring2.webp)'
+    background.style.backgroundImage = 'url(../../assets/spring2.webp)'
   }
   else if (state.tempValue >= 50){
-    backgroundImage = 'url(../../assets/spring.jpg)'
+    background.style.backgroundImage = 'url(../../assets/spring.jpg)'
   }
   else if (state.tempValue >= 40){
-    backgroundImage = 'url(../../assets/autumn.jpg)'
+    background.style.backgroundImage = 'url(../../assets/autumn.jpg)'
   }
   else if (state.tempValue >= 20){
-    backgroundImage = 'url(../../assets/winter-lanscape.webp)'
+    background.style.backgroundImage = 'url(../../assets/winter-lanscape.webp)'
   }
   else {
-    backgroundImage = 'url(../../assets/deepfreeze.jpg)'
+    background.style.backgroundImage = 'url(../../assets/deepfreeze.jpg)'
   }
-};
-
-const increaseTemp = () => {
-  state.tempValue += 1;
-  temperature.textContent = String(state.tempValue) + '°F';
-  updateBackground();
-  updateTempColor();
-};
-
-const decreaseTemp = () => {
-  state.tempValue -= 1;
-  temperature.textContent = String(state.tempValue +'°F');
-  updateBackground();
-  updateTempColor();
 };
 
 const updateTempColor = () => {
@@ -72,10 +58,19 @@ const updateTempColor = () => {
     temperature.classList = 'teal';
   }
 };
-const updateCityName = () => {
-let cityNameInput = document.getElementById('cityNameInput').value;
-let headerCityName = document.getElementById('headerCityName');
-headerCityName.textContent = cityNameInput;
+
+const increaseTemp = () => {
+  state.tempValue += 1;
+  temperature.textContent = String(state.tempValue) + '°F';
+  updateBackground();
+  updateTempColor();
+};
+
+const decreaseTemp = () => {
+  state.tempValue -= 1;
+  temperature.textContent = String(state.tempValue +'°F');
+  updateBackground();
+  updateTempColor();
 };
 
 const updateSky = () => {
@@ -96,6 +91,19 @@ const updateSky = () => {
   }
 };
 
+const updateCityName = () => {
+  let cityNameInput = document.getElementById('cityNameInput').value;
+  let headerCityName = document.getElementById('headerCityName');
+  state.city = inputName;
+  headerCityName.textContent = state.city
+  };
+  
+  const resetCityName = () => {
+    let cityNameInput = document.getElementById('cityNameInput');
+    cityNameInput.value = 'Seattle';
+    updateCityName();
+  };
+
 const getLatLon = () => {
   axios.get('https://localhost:5000/location', parameters = {'q': state.city})
       .then((response) => {
@@ -108,12 +116,6 @@ const getLatLon = () => {
 };
 
 getLatLon();
-
-const resetCityName = () => {
-  let cityNameInput = document.getElementById('cityNameInput');
-  cityNameInput.value = 'Seattle';
-  updateCityName();
-}
 
 const registerEventHandlers = () => {
 
