bd23b5fa321a7fc8e0c66bf16ad301c4e50306ae
Maria Silva <hello@mariasilva.dev>
2022-12-08 21:26:07 -0600

    able to call API and get lat and lon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// import axios from 'axios';
-
 let total_element = document.getElementById('Total');
 
 function Add () {
   let total_value = parseInt(total_element.innerHTML);
   total_element.innerHTML = total_value + 1;
-  changeLandscape(total_value);
+  let myTemp = total_value + 1;
+  changeLandscape(myTemp);
 }
 
 
@@ -13,47 +12,68 @@ function Subtract () {
   let total_value = parseInt(total_element.innerHTML);
   if (total_value == 0) return; 
   total_element.innerHTML = total_value - 1;
-  changeLandscape(total_value);
+  let myTemp = total_value - 1;
+  changeLandscape(myTemp);
 }
 
 function changeLandscape (temp) {
-  if (temp <= 59) {
-    document.getElementById('landscape').innerHTML = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
-  } else if (temp <= 69) {
-    document.getElementById('landscape').innerHTML = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
-  } else if (temp <= 79) {
-    document.getElementById('landscape').innerHTML = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
-  } else if (temp>= 80) {
+  if (temp >=80) {
     document.getElementById('landscape').innerHTML = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (temp >= 70) {
+    document.getElementById('landscape').innerHTML = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (temp >= 60) {
+    document.getElementById('landscape').innerHTML = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else {
+    document.getElementById('landscape').innerHTML = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
   }
 }
 
 function changeCityName () {
-  // let cityInput = document.getElementById('city').value;
-  document.getElementById('displaycity').innerHTML = document.getElementById('city').value;
+  let cityInput = document.getElementById('city').value;
+  document.getElementById('displayCity').innerHTML = cityInput;
+  return cityInput;
 }
 
-document.getElementById('city').addEventListener('input', changeCityName);
 
 document.getElementById('Add').addEventListener('click', Add);
 
 document.getElementById('Subtract').addEventListener('click', Subtract);
 
-// window.addEventListener("load", loadpage);
-// function cargaPagina() {
-//     var btn = document.getElementById("button1").addEventListener("click", changevalues);
-// }
-
-// function changevalues() {
-//     var inputName = document.getElementById("name");
-//     inputName.value = "Search City";
-// }
-
-// var x = document.getElementById("myText");
+document.getElementById('city').addEventListener('input', changeCityName);
 
 
+window.addEventListener("load", loadpage);
+function cargaPagina() {
+    var btn = document.getElementById("button1").addEventListener("click", changevalues);
+}
 
-// const axios = require('axios');
-// axios
-//   .get("https://us1.locationiq.com/v1/location")
+function changevalues() {
+    var inputName = document.getElementById("name");
+    inputName.value = "Search City";
+}
+
+var x = document.getElementById("myText");
+
+
+const findCityLatLon = () => {
+  let city = changeCityName();
+  axios
+    .get('http://127.0.0.1:5000/location', {
+      params: {
+        q: city,
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+      let lat = response.data[0].lat;
+      let lon = response.data[0].lon;
+      console.log(lat)
+      console.log(lon)
+    })
+    .catch((error) => {
+      console.log('Error given back by the API response:', error);
+    });
+}
+
+document.getElementById('getDisplayCityTemp').addEventListener('click', findCityLatLon);
 
