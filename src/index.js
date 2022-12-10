47b3820da7390e8912598baefb30ef8c62c8b8a8
Yael <yaelechols@gmail.com>
2022-12-10 14:54:39 -0800

    Implements logic for event handling and for state changes. Also creates logic for temperature increase and decrease.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+const state = {
+    increaseTempControl: null,
+    decreaseTempControl: null,
+    tempValue: 45,
+    tempDisplay: null
+};
+
+const loadControls = () => {
+    state.increaseTempControl = document.getElementById("increaseTempControl");
+    state.decreaseTempControl = document.getElementById("decreaseTempControl");
+    state.tempDisplay = document.getElementById("tempValue");
+};
+
+const handleIncreaseTempClicked = () => {
+    ++state.tempValue;
+    state.tempDisplay.textContent = state.tempValue;
+}
+
+const handleDecreaseTempClicked = () => {
+    --state.tempValue;
+    state.tempDisplay.textContent = state.tempValue;
+}
+
+const registerEvents = () => {
+    state.increaseTempControl.addEventListener("click", handleIncreaseTempClicked);
+    state.decreaseTempControl.addEventListener("click", handleDecreaseTempClicked);
+};
+
+const onLoaded = () => {
+    loadControls();
+    registerEvents();
+};
+
+onLoaded();
+
 ////////////////wave 3\\\\\\\\\\\\\\\\\\\
 
 const input = document.querySelector('input');
