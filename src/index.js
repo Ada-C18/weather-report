35db6d444383c21b3ebb705f7329e226b9e295bc
n2020h <uzame.va@gmail.com>
2022-12-10 16:54:21 -0500

    added js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,66 @@
-const state = {
+import axios from 'axios';
+import 'regenerator-runtime/runtime';
+
+const axios = require('axios');
+export const state = {
   city: 'Seattle',
   lat: 47.6038321,
   long: -122.3300624,
-  temp: 72,
+  temp: 73,
 };
 
-const formatTempAndGarden = () => {
+const updateCityName = () => {
+  const inputName = document.getElementById('cityNameInput').value;
+  const headerCityName = document.getElementById('headerCityName');
+  state.city = inputName;
+  headerCityName.textContent = state.city;
+};
+
+const resetCityName = () => {
+  const cityNameInput = document.getElementById('cityNameInput');
+  cityNameInput.value = 'Seattle';
+  updateCityName();
+};
+
+export const formatTempAndGarden = () => {
   let temp = state.temp;
   let color = 'red';
-  let landscapeSource = null;
-
+  let landscape = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
   if (temp > 80) {
     color = 'red';
-    landscapeSource = 'images/veryhot.webp';
     landscape = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
   } else if (temp > 70) {
     color = 'orange';
-    landscapeSource = 'images/spring.webp';
-    landscape = '🌸🌿🌼__🌷🌻🌿_🌱_🌻🌷';
+    landscape = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
   } else if (temp > 60) {
     color = 'yellow';
-    landscapeSource = 'images/fall.webp';
     landscape = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
   } else if (temp > 50) {
     color = 'green';
-    landscapeSource = 'images/winter.webp';
     landscape = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
   } else {
     color = 'teal';
-    landscapeSource = 'images/winter.webp';
     landscape = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
   }
+
   const newLandscape = document.getElementById('landscape');
   newLandscape.textContent = landscape;
-  const landscapeImage = document.getElementById('landscapeImg');
-  landscapeImage.src = landscapeSource;
   const temperature = document.getElementById('tempValue');
   temperature.className = color;
   temperature.textContent = String(state.temp);
 };
 
-const increaseTemp = () => {
+export const increaseTemp = () => {
   state.temp += 1;
   formatTempAndGarden();
 };
 
-const decreaseTemp = () => {
+export const decreaseTemp = () => {
   state.temp -= 1;
   formatTempAndGarden();
 };
 
-const registerEventHandlers = () => {
+export const registerEventHandlers = () => {
   formatTempAndGarden();
 
   const increaseTempControl = document.getElementById('increaseTempControl');
@@ -58,6 +68,13 @@ const registerEventHandlers = () => {
 
   const decreaseTempControl = document.getElementById('decreaseTempControl');
   decreaseTempControl.addEventListener('click', decreaseTemp);
+
+  const cityNameResetBtn = document.getElementById('cityNameReset');
+  cityNameResetBtn.addEventListener('click', resetCityName);
+
+  updateCityName();
+  const cityNameInput = document.getElementById('cityNameInput');
+  cityNameInput.addEventListener('input', updateCityName);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
