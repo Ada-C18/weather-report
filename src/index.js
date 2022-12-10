5e6cd85a452d6519376b9f437b576b50c0f87f7c
Makeesha Pruitt <makeeshalp@gmail.com>
2022-12-09 19:34:52 -0500

    completed wave 2

--- src/index.js
+++ src/index.js
@@ -3,24 +3,35 @@
 const state = {
     tempValue: 62,
     tempFontColor: "goldenrod",
-
+    nameOfCity: "Atlanta",
+    weatherGarden: "🌷🌷🌷🌷",
 };
 
 const tempValue = document.getElementById("tempValue");
 tempValue.textContent = `${state.tempValue}° F`;
 tempValue.style.color = state.tempFontColor;
 
+const nameOfCity = document.getElementById("location-city");
+nameOfCity.textContent = state.nameOfCity;
+
+const weatherGarden = document.getElementById("weather-garden");
+weatherGarden.textContent = state.weatherGarden;
+
 // Functions to increase or decrease temperature
 const increaseTemp = () => {
     state.tempValue += 1;
     tempValue.textContent = `${state.tempValue}° F`;
     changeTempFontColor();
+    changeWeatherGarden();
+
 };
 
 const decreaseTemp = () => {
     state.tempValue -= 1;
     tempValue.textContent = `${state.tempValue}° F`;
     changeTempFontColor();
+    changeWeatherGarden();
+    
 };
 // Function to determine color of the temperature font
 const changeTempFontColor = () =>   {
@@ -44,14 +55,44 @@ const changeTempFontColor = () =>   {
         tempValue.style.color = state.tempFontColor;
     }
 };
+// function to change weather garden
+const changeWeatherGarden = () => {
+    if (state.tempValue >= 80) {
+        state.weatherGarden = "🌴🌴🌴🌴";
+        weatherGarden.textContent = state.weatherGarden;
+    } else if (state.tempValue >= 60 & state.tempValue < 80) {
+        state.weatherGarden = "🌷🌷🌷🌷";
+        weatherGarden.textContent = state.weatherGarden;
+    } else if (state.tempValue >= 40 & state.tempValue < 60) {
+        state.weatherGarden = "🍁🍁🍁🍁";
+        weatherGarden.textContent = state.weatherGarden;
+    } else if (state.tempValue < 40) {
+        state.weatherGarden = "🥀🥀🥀🥀";
+        weatherGarden.textContent = state.weatherGarden;
+    }
+};
+
+// Function to change city input
+const inputCity = (userCityInput) => {
+    const locationCity = document.getElementById("location-city");
+    state.nameOfCity = userCityInput.target.value;
+    locationCity.textContent = state.nameOfCity;
+};
+
 
 // Registering Event Handlers
 const registerEventHandlers = () => {
-    const upButton = document.getElementById("increaseTempControl");
+    const upButton = document.getElementById("increase-temp-control");
     upButton.addEventListener("click", increaseTemp);
 
-    const downButton = document.getElementById("decreaseTempControl");
+    const downButton = document.getElementById("decrease-temp-control");
     downButton.addEventListener("click", decreaseTemp);
+
+    const cityInput = document.getElementById("city-content");
+    cityInput.addEventListener("input", inputCity);
+
+    
 };
+
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
 
