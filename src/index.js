8f64b3d9919e87fd413eced5d17e62f724f1069d
Minh <minh.seikel@gmail.com>
2022-12-09 17:29:21 -0800

    updating sky text content to reflect user choice. Update rounding to show rounding number when clicking up/down button after user get current temperature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
-
-
 // logic to change temperature using the buttons
 const state = {
   temp: 45,
@@ -13,20 +10,18 @@ console.log('Temperature is:', convertKelvinToF(271.54));
 
 //Creating addTemp and minus Temp events
 const addTemp = (event) => {
-  state.temp += 1;
+  state.temp = Math.round(state.temp + 1);
   const tempControl = document.querySelector('#currentTemp');
   tempControl.textContent = state.temp;
   changeTempNumColor();
 };
 
 const minusTemp = (event) => {
-  state.temp -= 1;
+  state.temp = Math.round(state.temp - 1);
   const tempControl = document.querySelector('#currentTemp');
   tempControl.textContent = state.temp;
   changeTempNumColor();
 };
-
-
 
 //HELPER FUNCTION logic for temperture to change color font, landscape and sky
 const changeTempNumColor = () => {
@@ -60,82 +55,87 @@ const changeTempNumColor = () => {
 ////Creating an event to update headerCityName
 const updateCityName = (event) => {
   const updatedCityName = document.getElementById('headerCityName');
-  state.city = event.target.value
+  state.city = event.target.value;
   updatedCityName.textContent = `🌍🌟${event.target.value}🌟🌍`;
-  console.log("New city name", state.city)
-  return state.city
+  console.log('New city name', state.city);
+  return state.city;
 };
 
 //Get Current temperature button event
 const getCurrentTemp = (event) => {
   //call the API to return the current temp
   //take the current temp and update the state.temp
-  getLatAndLon(state.city)  
-  console.log("inside get current temp");
-
-}
-
-
+  getLatAndLon(state.city);
+  console.log('inside get current temp');
+};
 
 //API call for LocationIQ
-locationURL = 'http://127.0.0.1:5000/location'
-weatherURL = 'http://127.0.0.1:5000/weather'
+locationURL = 'http://127.0.0.1:5000/location';
+weatherURL = 'http://127.0.0.1:5000/weather';
 
 // const location = updateCityName();
 const getLatAndLon = (location) => {
-  axios.get(locationURL,{
-    params:{
-      q:state.city,
-      format: 'json'
-    }
-  }).then((result) => {
-    const lat = result.data[0].lat;
-    const lon = result.data[0].lon;
-    console.log(`${location} lat: ${lat} lon: ${lon}`);
-
-    axios.get(weatherURL,{
-      params:{
-        lat:lat,
-        lon:lon
-      }
-    }).then((response) => {
-      console.log(convertKelvinToF(response.data.main.temp));
-      state.temp = convertKelvinToF(response.data.main.temp);
-      //modify the current temp number
-      const currentTempReal = document.querySelector('#currentTemp');
-      currentTempReal .textContent = Math.round(state.temp);
-      changeTempNumColor();
-
-
-    }).catch((error)=>{
-      console.log(error);
+  axios
+    .get(locationURL, {
+      params: {
+        q: state.city,
+        format: 'json',
+      },
     })
+    .then((result) => {
+      const lat = result.data[0].lat;
+      const lon = result.data[0].lon;
+      console.log(`${location} lat: ${lat} lon: ${lon}`);
 
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  
+      axios
+        .get(weatherURL, {
+          params: {
+            lat: lat,
+            lon: lon,
+          },
+        })
+        .then((response) => {
+          console.log(convertKelvinToF(response.data.main.temp));
+          state.temp = convertKelvinToF(response.data.main.temp);
+          //modify the current temp number
+          const currentTempReal = document.querySelector('#currentTemp');
+          currentTempReal.textContent = Math.round(state.temp);
+          changeTempNumColor();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
+
 
 //create an event to take user's selection
 //take user selection and give if/else statement
+//12.9 changes:
+//1) updating the sky to new emojis
 const skySelect = () => {
-  let selection = document.getElementById("selectSky")
-  // console.log(selection.value)
-  // const userChange = () => {
-  //   let userChoice = selection.value;
-  // }
-  let userChoice = selection.value
-  if (userChoice = "Rainy") {
-    const gardenBackground = document.getElementById("#garden_section")
-    gardenBackground.classList.add("rainy")
+  let selection = document.getElementById('selectSky');
 
+  let userChoice = selection.value;
+  console.log("select select select")
+  if (userChoice == 'Rainy') {
+    const gardenSky =
+      document.getElementById("sky");
+    gardenSky.textContent = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
+  } else if (userChoice == 'Sunny') {
+    const gardenSky = document.getElementById("sky");
+    gardenSky.textContent = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️";
+  } else if (userChoice == 'Cloudy') {
+    const gardenSky = document.getElementById("sky");
+    gardenSky.textContent = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
+  } else if (userChoice == 'Snowy') {
+    const gardenSky = document.getElementById("sky");
+    gardenSky.textContent = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨";
   }
-
 };
-//add event listener
-
 
 
 ////Registering Event handlers and add event listeners
@@ -151,11 +151,10 @@ const registerEventHandlers = (event) => {
   input.addEventListener('input', updateCityName);
 
   const updateCurrentTempButton = document.querySelector('#getCurrentTemp');
-  updateCurrentTempButton.addEventListener('click', getCurrentTemp)
+  updateCurrentTempButton.addEventListener('click', getCurrentTemp);
 
   const updateSky = document.querySelector('#selectSky');
-  updateSky.addEventListener('change', skySelect)
+  updateSky.addEventListener('change', skySelect);
 };
-
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
