0b02c947c6a43cf9d5954d6eedbd579917e51808
Eve Stroud <evestroud89@gmail.com>
2022-12-11 15:00:15 -0500

    Add city reset button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const cityName = document.querySelector('#city-name');
 const citySelector = document.querySelector('#city-selector');
+const cityReset = document.querySelector('#city-reset');
 const tempBox = document.querySelector('#temp-box');
 const temperature = document.querySelector('#temp');
 const tempUnit = document.querySelector('#temp-unit');
@@ -11,6 +12,11 @@ const landscape = document.querySelector('#landscape');
 const buttonIncrease = document.querySelector('#button-increase');
 const buttonDecrease = document.querySelector('#button-decrease');
 const unitSelector = document.querySelector('#unit-selector');
+
+cityReset.addEventListener('click', (_) => {
+    citySelector.value = '';
+    updateLocation('Atlanta');
+});
 
 buttonIncrease.addEventListener('click', (_) => {
     State.temperature += 1;
