f5bb04313012a53df0ea8c4108b41d576d9d8bec
Yael <yaelechols@gmail.com>
2022-12-10 17:21:43 -0800

    Implements landscape change logic for tempValue state changes

--- src/index.js
+++ src/index.js
@@ -2,25 +2,29 @@ const state = {
     increaseTempControl: null,
     decreaseTempControl: null,
     tempValue: 45,
-    tempDisplay: null
+    tempDisplay: null,
+    gardenContent: '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲'
 };
 
 const loadControls = () => {
-    state.increaseTempControl = document.getElementById("increaseTempControl");
-    state.decreaseTempControl = document.getElementById("decreaseTempControl");
-    state.tempDisplay = document.getElementById("tempValue");
+    state.increaseTempControl = document.getElementById('increaseTempControl');
+    state.decreaseTempControl = document.getElementById('decreaseTempControl');
+    state.tempDisplay = document.getElementById('tempValue');
+    state.gardenContent = document.getElementById('gardenContent');
 };
 
 const handleIncreaseTempClicked = (event) => {
     ++state.tempValue;
     state.tempDisplay.textContent = state.tempValue;
     handleChangeColor(state.tempValue);
+    handleChangeLandscape(state.tempValue);
 }
 
 const handleDecreaseTempClicked = (event) => {
     --state.tempValue;
     state.tempDisplay.textContent = state.tempValue;
     handleChangeColor(state.tempValue);
+    handleChangeLandscape(state.tempValue);
 }
 
 const handleChangeColor = (event) => {
@@ -47,10 +51,28 @@ const handleChangeColor = (event) => {
     state.tempDisplay.className = 'teal';
 }
 
+const handleChangeLandscape = (event) => {
+    if (state.tempValue >= 80) {
+        state.gardenContent.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+        return;
+    }
+
+    if (state.tempValue >= 70) {
+        state.gardenContent.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+        return;
+    }
+
+    if (state.tempValue >= 60) {
+        state.gardenContent.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+        return;
+    }
+
+    state.gardenContent.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+}
+
 const registerEvents = () => {
-    state.increaseTempControl.addEventListener("click", handleIncreaseTempClicked);
-    state.decreaseTempControl.addEventListener("click", handleDecreaseTempClicked);
-    // state.tempValue.addEventListener("change", handleChangeColor)
+    state.increaseTempControl.addEventListener('click', handleIncreaseTempClicked);
+    state.decreaseTempControl.addEventListener('click', handleDecreaseTempClicked);
 };
 
 const onLoaded = () => {
