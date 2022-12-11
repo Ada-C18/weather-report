37337f958fcc414cdf5ecb0fb59e889b40cf4186
Elsje Slothower <elsje.slothower@gmail.com>
2022-12-11 14:54:35 -0800

    wave 2 complete

--- src/index.js
+++ src/index.js
@@ -28,21 +28,38 @@ const tempChange = () => {
 
 const landscapeChange = () => {
   let temp = state.temp;
+  let landscape = "hot!"
+  if (temp > 80) {
+    landscape = "hot hot!";
+  } else if (temp > 70) {
+    landscape = "warmm";
+  } else if (temp > 60) {
+    landscape = "nice";
+  } else if (temp > 50) {
+    landscape = "cool";
+  } else {
+    landscape = "**shiver shiver**";
+  }
 
+  const updatedLandscape = document.getElementById("skyAndLandscape");
+  updatedLandscape.textContent = landscape;
 }
 
 const increasedTemp = () => {
   state.temp += 1;
   tempChange();
+  landscapeChange();
 }
 
 const decreasedTemp = () => {
   state.temp -= 1;
   tempChange();
+  landscapeChange();
 }
 
 const registerEventHandlers = () => {
   tempChange();
+  landscapeChange();
 
   const increasedTempButton = document.getElementById("increaseTemp");
   increasedTempButton.addEventListener("click", increasedTemp);
