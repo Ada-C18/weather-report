4cd0a82e0d40cdeee2ca1d8e5e8eedcb826d88e4
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-12-11 14:14:46 -0800

    Adds background images and changing background feature and temp color change feature

--- src/index.js
+++ src/index.js
@@ -9,27 +9,35 @@ let counter = 0;
 
 increaseBtn.addEventListener('click', ()=>{
   counter++;
+  console.log("increase clicked", counter)
+  counterColor();
   curTemp.innerHTML = counter;
 });
 
 decreaseBtn.addEventListener('click', ()=>{
   counter--;
+  console.log("decrease clicked", counter)
+  counterColor()
   curTemp.innerHTML = counter
 });
 
-// const counterColor = () =>{
-//   if (counter >= 80){
-//     curTemp.classList.add("red");
-//   } else if (counter <= 79) {
-//     curTemp.classList.add("orange");
-//   } else if (counter <= 69) {
-//     curTemp.classList.add("yellow");
-//   } else if (counter <= 59) {
-//     curTemp.classList.add("green");
-//   } else if (counter <= 49) {
-//     curTemp.classList.add("teal");
-//   }
-// };
+function myFunction() {
+  const x = document.getElementById("cityInput").value;
+  document.getElementById("demo").innerHTML = x;
+}
+
+const counterColor = () =>{
+  if (counter <= 49) {
+  curTemp.className="teal";
+  } else if (counter <= 59) {
+  curTemp.className="green";
+  } else if (counter <= 69) {
+  curTemp.className="yellow";
+  } else if (counter <= 79) {
+    curTemp.className="orange";
+  } else {
+      curTemp.className="red";
+}};
 
 const cityName = () =>{
   const city = document.getElementById("cityInput").value;
@@ -38,4 +46,24 @@ const cityName = () =>{
 
 };
 
-document.addEventListener('click', cityName)
+document.getElementById("sky").addEventListener('change', (event) => {
+  console.log("changed", event)  
+  const bgValue = event.target.options[event.target.selectedIndex].value;
+  console.log(bgValue)  
+  if( bgValue == 'sunny' ) {
+    document.body.style.backgroundImage = "url(/ada-project-docs/assets/sunshine.png)";      
+    } else if( bgValue == "cloudy" ){
+      document.body.style.backgroundImage = "url(/ada-project-docs/assets/cloudy.png)";      
+    } else if( bgValue == "rainy" ){
+      document.body.style.backgroundImage = "url(/ada-project-docs/assets/rain.png)";      
+    } else if( bgValue == "snowy" ){
+      document.body.style.backgroundImage = "url(/ada-project-docs/assets/snow.png)";      
+    }
+});
+
+document.body.style.backgroundImage = "url('/ada-project-docs/assets/sunshine.png')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
+// document.body.style.maskImage = linear-gradient(rgba(1, 1, 1, 1), transparent);
+
+
