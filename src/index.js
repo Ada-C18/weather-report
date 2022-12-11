677c28d0ffa994fd52a7a967d3385749d27b641a
Eve Stroud <evestroud89@gmail.com>
2022-12-11 08:19:42 -0500

    Switch index.js from tabs to spaces

--- src/index.js
+++ src/index.js
@@ -12,59 +12,58 @@ const buttonIncrease = document.querySelector('#button-increase');
 const buttonDecrease = document.querySelector('#button-decrease');
 
 buttonIncrease.addEventListener('click', (_) => {
-	State.temperature += 1;
-	updateWeather();
+    State.temperature += 1;
+    updateWeather();
 });
 
 buttonDecrease.addEventListener('click', (_) => {
-	State.temperature -= 1;
-	updateWeather();
+    State.temperature -= 1;
+    updateWeather();
 });
 
 citySelector.addEventListener('input', (_) => {
-	// cityName.textContent = citySelector.value;
-	axios
-		.get('http://127.0.0.1:5000/location', {
-			params: {
-				q: citySelector.value,
-				format: 'json',
-			},
-		})
-		.then((response) => {
-			console.log(response.data[0]);
-			State.latitude = response.data[0]['lat'];
-			State.longitude = response.data[0]['lon'];
-			cityName.textContent = response.data[0]['display_name'];
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+    axios
+        .get('http://127.0.0.1:5000/location', {
+            params: {
+                q: citySelector.value,
+                format: 'json',
+            },
+        })
+        .then((response) => {
+            console.log(response.data[0]);
+            State.latitude = response.data[0]['lat'];
+            State.longitude = response.data[0]['lon'];
+            cityName.textContent = response.data[0]['display_name'];
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 });
 
 const State = {
-	temperature: 70,
-	unit: 'F',
-	latitude: 0,
-	longitude: 0,
+    temperature: 70,
+    unit: 'F',
+    latitude: 0,
+    longitude: 0,
 };
 
 const updateWeather = function() {
-	temperature.textContent = `${State.temperature}`;
-	tempUnit.textContent = `${State.unit}`;
+    temperature.textContent = `${State.temperature}`;
+    tempUnit.textContent = `${State.unit}`;
 
-	if (State.temperature < 32) {
-		landscape.className = 'landscape-cold';
-		tempBox.className = 'temp-box-cold';
-	} else if (State.temperature < 60) {
-		landscape.className = 'landscape-cool';
-		tempBox.className = 'temp-box-cool';
-	} else if (State.temperature < 90) {
-		landscape.className = 'landscape-warm';
-		tempBox.className = 'temp-box-warm';
-	} else {
-		landscape.className = 'landscape-hot';
-		tempBox.className = 'temp-box-hot';
-	}
+    if (State.temperature < 32) {
+        landscape.className = 'landscape-cold';
+        tempBox.className = 'temp-box-cold';
+    } else if (State.temperature < 60) {
+        landscape.className = 'landscape-cool';
+        tempBox.className = 'temp-box-cool';
+    } else if (State.temperature < 90) {
+        landscape.className = 'landscape-warm';
+        tempBox.className = 'temp-box-warm';
+    } else {
+        landscape.className = 'landscape-hot';
+        tempBox.className = 'temp-box-hot';
+    }
 };
 
 updateWeather();
