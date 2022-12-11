e4d2b57fe70d79e17b87440f16af309481fa3fde
ashanti <ammanier2@gmail.com>
2022-12-11 17:04:18 -0500

    added get weather API call

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { default: axios } = require("axios");
+
 // const changeTemp = tempDisplay => {
 //   const 
 // }
@@ -10,22 +12,40 @@
 //   changeTemp(tempDisplay)
 // }
 
-
-const getLocation = () => {
-  axios
-  .get ('http://127.0.0.1:5000/location'), {
-    params: {
-      key: 'pk.b8aab1a85295b2c1f6e71a4ed20c3120',
-      q: loc_query,
-      format: 'json'
+const weatherforecast = (city_name) => {
+  const getLocation = () => {
+    axios
+    .get ('http://127.0.0.1:5000/location'), {
+      params: {
+        key: 'pk.b8aab1a85295b2c1f6e71a4ed20c3120',
+        q: city_name,
+        format: 'json'
+      }
     }
-  }
-  .then (function(response){
-    let lat = response.data[0]["lat"];
-    let long = response.data[0]["lon"];
-  })
+    .then (function(response){
+      let lat = response.data[0]["lat"];
+      let lon = response.data[0]["lon"];
+      getWeatherFromLoc(lat, lon);
+    })
 
-  .catch (function(error) {
-    console.error(error);
-  })
+    .catch (function(error) {
+      console.error(error);
+    })
+  }
+
+  const getWeatherFromLoc = (lat, lon) => {
+    axios
+    .get ('http://127.0.0.1:5000/weather'), {
+      params: {
+        appid: '76d80595e856e8e068fcdd93241e2622',
+        lat: lat,
+        lon: lon
+      }
+    }
+    .then (function(response){
+      let kelvinTemp = response.data['main']['temp'];
+      let farhTemp = Math.floor((kelvinTemp - 273.15) * 1.8) + 32;
+      // VARIABLE.innerText = farhTemp
+    })
+  }
 }
