d2519731fec85b2a91a971bfa13242cadb00102e
ashanti <ammanier2@gmail.com>
2022-12-11 16:19:03 -0500

    connected proxy server to APIs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// const changeTemp = tempDisplay => {
+//   const 
+// }
+
+
+// const increaseTemp = () => {
+//   tempDisplay += 1;
+//   changeTemp(tempDisplay)
+// }
+
+
+const getLocation = () => {
+  axios
+  .get ('http://127.0.0.1:5000/location'), {
+    params: {
+      key: 'pk.b8aab1a85295b2c1f6e71a4ed20c3120',
+      q: loc_query,
+      format: 'json'
+    }
+  }
+  .then (function(response){
+    let lat = response.data[0]["lat"];
+    let long = response.data[0]["lon"];
+  })
+
+  .catch (function(error) {
+    console.error(error);
+  })
+}
