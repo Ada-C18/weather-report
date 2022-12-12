dc93e488888b73fc29bb801094b99e358d2f4f3e
LP <lp.passarelli.wilson@gmail.com>
2022-12-12 13:02:04 -0800

    got axios call to weather report working woooooo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const axios = require('axios');
-const dotenv = require('dotenv').config();
-const API_KEY = process.env.API_KEY;
+//const axios = require('axios');
+//const dotenv = require('dotenv').config();
+//const API_KEY = process.env.API_KEY;
 
 ('use strict');
+//call to the flask app to get weather
+const lat = 47.6038321;
+const lon = -122.330062;
+
+//later, change
+axios
+  .get('http://127.0.0.1:5000/weather' + '?lat=' + lat + '&lon=' + lon)
+  .then((response) => {
+    console.log(response);
+  });
+
+//if loading,
