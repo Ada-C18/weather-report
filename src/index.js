88d9349551056d08eb90b297178ee18b9655d2e5
Jennifer <datateur@gmail.com>
2022-12-11 18:56:49 -0800

    Refactoring

--- src/index.js
+++ src/index.js
@@ -27,28 +27,31 @@ const updateBackground = () => {
   const backgroundImage = document.getElementById('bg').style.backgroundImage;
 
   if (state.tempValue > 100){
-    backgroundImage = 'url(assets/sun-surface.jpg)'
+    backgroundImage = 'url(assets/sun-surface.jpg)';
   }
   else if (state.tempValue > 90){
-    backgroundImage = 'url(assets/desert.jpg)'
+    backgroundImage = 'url(assets/desert.jpg)';
   }
   else if (state.tempValue > 80){
-    backgroundImage = 'url(assets/desert1.jpg)'
+    backgroundImage = 'url(assets/desert1.jpg)';
   }
   else if (state.tempValue > 70){
-    backgroundImage = 'url(assets/summer.jpg)'
+    backgroundImage = 'url(assets/summer.jpg)';
   }
   else if (state.tempValue > 60){
-    backgroundImage = 'url(assets/spring.jpg)'
+    backgroundImage = 'url(assets/spring.jpg)';
   }
   else if (state.tempValue > 50){
-    backgroundImage = 'url(assets/spring2.webp)'
+    backgroundImage = 'url(assets/spring2.webp)';
   }
   else if (state.tempValue > 40){
-    backgroundImage = 'url(assets/autumn.jpg)'
+    backgroundImage = 'url(assets/autumn.jpg)';
+  }
+  else if (state.tempValue > 30){
+    backgroundImage = 'url(assets/winter-lanscape.webp)';
   }
   else if (state.tempValue > 20){
-    backgroundImage = 'url(assets/winter-lanscape.webp)'
+    backgroundImage = 'url(assets/deepfreeze.jpg)';
   }
 };
 
