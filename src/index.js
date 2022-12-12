322ab96f32c996e17fdf5f43d5992a6076b9c958
Nicole Mejia <snicolemejia@gmail.com>
2022-12-11 19:28:30 -0500

    added fiveDayForecast request

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ const state = {
   clicked: false, // whether the temp increase button is in clicked state
 };
 
-// EVENT HANDLERS
+const forecastState  = {
+  1: {day: "", main: "", temp: 0, humidity: 0,},
+  2: {day: "", main: "", temp: 0, humidity: 0,},
+  3: {day: "", main: "", temp: 0, humidity: 0,},
+  4: {day: "", main: "", temp: 0, humidity: 0,},
+  5: {day: "", main: "", temp: 0, humidity: 0,},
+};
 
+// EVENT HANDLERS
 // INCREASE TEMP BY 1 DEGREE
 const increaseDreamTemp = () => {
   const dreamTemp = document.getElementById("dream-temp-number");
@@ -64,11 +71,10 @@ const updateDreamTempColor = () => {
 const changeCityNameWithInput = () => {
   const cityTitle = document.getElementById("city-title");
   const cityInput = document.getElementById("search-bar").value;
-
   cityTitle.textContent = cityInput;
 };
 
-// AXIOS REQUESTS
+// AXIOS REQUEST
 // REQUEST LOCATION AND WEATHER FOR CITY FROM WEATHER PROXY SERVER
 const getCurrentWeather = () => {
   let cityInput = state.city;
@@ -80,9 +86,9 @@ const getCurrentWeather = () => {
   return axios.get('http://127.0.0.1:5000/location', {params: {q: cityInput}
   })
   .then(location => {
-    console.log("location:", location);
-    console.log("lat", location.data[0].lat);
-    console.log("lon", location.data[0].lon)
+    // console.log("location:", location);
+    // console.log("lat", location.data[0].lat);
+    // console.log("lon", location.data[0].lon)
     return {
       lat: location.data[0].lat,
       lon: location.data[0].lon
@@ -103,32 +109,108 @@ const getCurrentWeather = () => {
   })
 };
 
+// STORE CURRENT WEATHER REPORT AFTER ONE CALL
+const saveCurrentWeather = () => {
+    getCurrentWeather()
+    .then(weather => {
+    state.temp = Math.floor(weather.temp);
+    state.humidity = weather.humidity;
+    updateTemp()
+    updateHumidity()
+  });
+};
+
+// AXIOS REQUEST
+// REQUEST LOCATION AND FORECAST FOR CITY FROM WEATHER PROXY SERVER
+const getFiveDayForecast = () => {
+  let cityInput = state.city;
+
+  if (document.getElementById("search-bar").value) {
+    cityInput = document.getElementById("search-bar").value;
+  };
+
+  return axios.get('http://127.0.0.1:5000/location', {params: {q: cityInput}
+  })
+  .then(location => {
+    console.log("location:", location);
+    console.log("lat", location.data[0].lat);
+    console.log("lon", location.data[0].lon)
+    return {
+      lat: location.data[0].lat,
+      lon: location.data[0].lon
+    }
+  })
+  .then(coordinates => {
+    console.log("coordinates.lat:", coordinates.lat);
+    console.log("coordinates.lon:", coordinates.lon)
+    return axios.get('http://127.0.0.1:5000/forecast', {
+                      params: 
+                      {lat: coordinates.lat,
+                      lon: coordinates.lon}
+                    })
+  })
+  .then(forecast => {
+    // console.log("forecast.data.list:", forecast.data.list);
+    const fiveDayForecast = [];
+    for (let day of forecast.data.list) {
+      let time = day.dt_txt.substring(11);
+      if (time === "12:00:00") {
+        fiveDayForecast.push(
+          {
+            date: day.dt_txt,
+            main: day.weather[0].main,
+            temp: day.main.temp,
+            humidity: day.main.humidity,
+          }
+        );
+      }
+    };
+    console.log(fiveDayForecast);
+    return fiveDayForecast;
+  })
+};
+
+// STORE CURRENT FIVE DAY FORECAST AFTER ONE CALL
+const saveFiveDayForecast= () => {
+  getFiveDayForecast()
+  .then(forecast => {
+    for (let i = 0; i < forecast.length; i++) {
+      forecastState[i+1] = {
+        day: forecast[i].date,
+        main: forecast[i].main,
+        temp: forecast[i].temp,
+        humidity: forecast[i].humidity
+      }
+    };
+    console.log(forecastState);
+  });
+};
+
 // UPDATE TEMP TO CURRENT TEMP FROM WEATHER RESULT
 const updateTemp = () => {
   const defaultTemp = document.getElementById("dream-temp-number");
-
-  getCurrentWeather()
-  .then(currentWeather => {
-    state.temp = kelvinToFahrenheit(currentWeather.temp);
-    defaultTemp.textContent = state.temp;
-    updateDreamTempColor();
-  });
+  defaultTemp.textContent = state.temp;
+  updateDreamTempColor();
 };
 
 // UPDATE HUMIDITY TO CURRENT HUMIDITY FROM WEATHER RESULT
 const updateHumidity = () => {
   const defaultHumidity = document.getElementById("dream-humidity-percent");
-  
-  getCurrentWeather()
-  .then(currentWeather => {
-    state.humidity = currentWeather.humidity
-    defaultHumidity.textContent = state.humidity;
-  });
+  defaultHumidity.textContent = state.humidity;
 };
 
-// CONVERT KELVIN TEMP TO FAHRENHEIT
-const kelvinToFahrenheit = (temp) => {
-  return Math.floor(((temp-273.15)*1.8)+32);
+// UPDATE WEATHER PANELS
+const updateWeatherPanels = () => {
+  // loop 5 times (total of panels)
+  // use i to determine which panel
+  // update name, temp and humidity for each panel
+  // grab from forecastState
+
+  for (let i = 1; i <= 5; i++) {
+    panel = document.getElementById(`panel-${i}`);
+    temp = document.getElementById(`panel-${i}`);
+    humidity = document.getElementById(`panel-${i}`);
+  }
 };
 
 // REGISTER EVENT HANDLERS
@@ -151,12 +233,12 @@ const registerEventHandlers = () => {
   searchBar.addEventListener("input", changeCityNameWithInput);
   
   const searchButton = document.getElementById("search-button");
-  searchButton.addEventListener("click", getCurrentWeather);
-  searchButton.addEventListener("click", updateTemp)
-  searchButton.addEventListener("click", updateHumidity)
+  // searchButton.addEventListener("click", getFiveDayForecast);
+  searchButton.addEventListener("click", updateTemp);
+  searchButton.addEventListener("click", updateHumidity);
 
-  window.addEventListener("load", updateTemp);
-  window.addEventListener("load", updateHumidity);
+  window.addEventListener("load", saveCurrentWeather);
+  window.addEventListener("load", saveFiveDayForecast);
 };
 
 // DOM listener
