d36da953b8f093fc95cf32c1a3b410efc99d87ee
LP <lp.passarelli.wilson@gmail.com>
2022-12-12 13:33:52 -0800

    made the conversion to farenheit in the code.  rounded the number down.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const lon = -122.330062;
 axios
   .get('http://127.0.0.1:5000/weather' + '?lat=' + lat + '&lon=' + lon)
   .then((response) => {
-    const temp = response.data.main.temp;
+    var temp = response.data.main.temp;
+    temp = Math.round(((temp - 273.15) * 9) / 5 + 32); //convert to Farenheit
     console.log(temp);
     const appearanceHeading = document.getElementById('temperaturenumber');
     appearanceHeading.textContent = temp + ' °F';
