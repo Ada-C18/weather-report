453fda05db22803e81f34ec2831c36c88d40d5cf
LP <lp.passarelli.wilson@gmail.com>
2022-12-13 09:53:04 -0800

    with jasmine's help, I did a lot of debugging.  i got the button click to be registered.  then i got event handlers to be called (before, it wasn't being called

--- src/index.js
+++ src/index.js
@@ -2,25 +2,47 @@
 //const dotenv = require('dotenv').config();
 //const API_KEY = process.env.API_KEY;
 
-('use strict');
-//call to the flask app to get weather
-const lat = 47.6038321;
-const lon = -122.330062;
-
-//later, change
-axios
-  .get('http://127.0.0.1:5000/weather' + '?lat=' + lat + '&lon=' + lon)
-  .then((response) => {
-    var temp = response.data.main.temp;
-    temp = Math.round(((temp - 273.15) * 9) / 5 + 32); //convert to Farenheit
-    console.log(temp);
-    const appearanceHeading = document.getElementById('temperaturenumber');
-    appearanceHeading.textContent = temp + ' °F';
-  });
+//CODE FOR BUTTON:
 
 const state = {
-  tempCount: temp,
+  tempCount: 60, //was temp
 };
+
+const addTemp = (event) => {
+  state.tempCount += 1;
+  console.log('the button was clicked!!!');
+};
+
+const registerEventHandlers = (event) => {
+  // console.log('in registerEventHandelers:', event);
+  const upButton = document.querySelector('#Up');
+  console.log(upButton);
+  upButton.addEventListener('click', addTemp);
+};
+
+registerEventHandlers(undefined);
+
+//THIS WAS UNCOMMENTED. IM COMMENTING IT OUT TO GET THE BUTTONS WORKING FIRST
+
+// ('use strict');
+// //call to the flask app to get weather
+// const lat = 47.6038321;
+// const lon = -122.330062;
+// axios
+//   .get('http://127.0.0.1:5000/weather' + '?lat=' + lat + '&lon=' + lon)
+//   .then((response) => {
+//     var temp = response.data.main.temp;
+//     temp = Math.round(((temp - 273.15) * 9) / 5 + 32); //convert to Farenheit
+//     console.log(temp);
+//     const appearanceHeading = document.getElementById('temperaturenumber');
+//     appearanceHeading.textContent = temp + ' °F';
+//   });
+
+// async function get_temp(query) {
+//   let response = await axios.get;
+// }
+
+//END UNCOMMENTED STUFF -----------
 
 // adding temperature number
 // async function f() {
@@ -29,20 +51,6 @@ const state = {
 // async function addTemp(event) {
 //   state.tempCount += 1;
 // }
-
-const addTemp = (event) => {
-  state.tempCount += 1;
-};
-
-const registerEventHandlers = (event) => {
-  // console.log('in registerEventHandelers:', event);
-  const upButton = document.querySelector('#Up');
-  upButton.addEventListener('click', addtemp);
-};
-
-async function get_temp(query) {
-  let response = await axios.get;
-}
 
 // const getPromise = (time) => {
 //   const timeoutTime = time * 1000;
