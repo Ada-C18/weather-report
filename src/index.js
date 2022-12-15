a85e2f9a52e84e7375cb4ef9de6bd9009d8c16c6
Anika Stephen Wilbur <anika.m.stephen@gmail.com>
2022-12-15 08:02:01 -0800

    Reset button working again and default value of Portland, OR added. Realtime temp still not working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 "use strict";
 
 const state = {
-  city: 'Portland',
-  temperature: 40,
-  latitude: 45.51,
-  longitude: 122.68,
+  city: 'Portland, OR',
+  temperature: 72,
+  latitude: 45.5152,
+  longitude: 122.6784,
 }
 
-let getcityName = document.getElementById("cityInput");
+let getCityName;
 let curWeatherEmojis = document.getElementById("curWeatherEmojis");
+let cityDisplayOnLoad = document.getElementById("now-showing").textContent = 
+  `Now showing the temperature for ${state.city}`;
 
-function myFunction (){
-  const input = document.getElementById("cityInput").value;
-  document.getElementById("cityHeader").textContent = input;
+const updateCityWhileTyping = () => {
+  const input = document.getElementById("defaultCity").value;
+  document.getElementById("now-showing").textContent = 
+  `Now showing the temperature for ${input}`
+  state.city = input;
 }
 
 document.getElementById("sky").addEventListener('change', (event) => {
@@ -55,31 +59,31 @@ const weatherEmojisandColor = () => {
     let tempTag = document.getElementById("tempTag")
     
     tempTag.textContent = String(temp)
-    tempTag.className = color
-    
+    tempTag.className = color   
 };
 
-const getLatLon = () => {
-  return axios.get("http://127.0.0.1:5000/location", {
-    params: {
-      q: state.city,
-    }
-  }).then((response)=>{
+const getLatLonAsync = async () => {
+  try {
+    const response = await axios.get("http://127.0.0.1:5000/location", {
+      params: {
+        q: state.city,
+      }}
+    )
+    console.log(state.city)
+    console.log("success!", response.data)
     state.latitude = response.data[0].lat;
     state.longitude = response.data[0].lon;
-
-    getWeather()
-    console.log("working")
-}).catch((error) => {
-    console.log("City not found")
-  })
+    getWeather();
+  } catch (error) {
+    console.log("City not found");
+  }
 };
 
 const getWeather = () => {
   axios.get("http://127.0.0.1:5000/weather", {
     params: {
-    lat: state.latitude,
-    lon: state.longitude,
+      lat: state.latitude,
+      lon: state.longitude,
   }})
   .then((response) => {
     const tempInF = Math.floor((response.data.main.temp - 273.15) * (9/5) + 32)
@@ -100,13 +104,6 @@ const decreaseTemp = () => {
     weatherEmojisandColor();
 }
 
-const adjustCityName = () => {
-  const inputCity = document.getElementById("cityInput").value;
-  const cityHeader = document.getElementById("cityHeader");
-  state.city = inputCity
-  cityHeader.textContent = state.city;
-};
-
 const adjustTemp = () => {
   const tempTag = document.getElementById("tempTag");
   getLatLon().then((temperature)=>{;
@@ -116,33 +113,39 @@ const adjustTemp = () => {
   })
 }
 
-const resetCity = () =>{
-  const cityInput = document.getElementById("cityInput");
-  cityInput.value = "Portland";
-
-  adjustCityName()
+const resetCity = () => {
+  const defaultCity = document.getElementById("defaultCity");
+  defaultCity.value = "Portland, OR";
+  state.city = defaultCity.value;
+  nowShowingTemp();
 }
+
+const nowShowingTemp = () => {
+  document.getElementById("now-showing").textContent = 
+    `Now showing the temperature for ${state.city}`;
+}; 
 
 // Section for Event Handlers 
 
 const registerEventHandlers = () => {
-  weatherEmojisandColor()
-
+  weatherEmojisandColor();
   const realTimeTemp = document.getElementById("realTime");
-  realTimeTemp.addEventListener('click', getLatLon)
+  realTimeTemp.addEventListener('click', getLatLonAsync);
   
-  const increaseBtn = document.getElementById("increaseBtn")
+  const increaseBtn = document.getElementById("increaseBtn");
   increaseBtn.addEventListener('click', increaseTemp);
   
-  const decreaseBtn = document.getElementById("decreaseBtn")
-  decreaseBtn.addEventListener('click', decreaseTemp)
+  const decreaseBtn = document.getElementById("decreaseBtn");
+  decreaseBtn.addEventListener('click', decreaseTemp);
 
-  adjustCityName()
-  getcityName.addEventListener('input', adjustCityName) 
+  const getCityName = document.getElementById("now-showing");
+  getCityName.addEventListener('input', updateCityWhileTyping);
 
   const resetBtn = document.getElementById("reset");
-  resetBtn.addEventListener('click', resetCity)
+  resetBtn.addEventListener('click', resetCity);
 
+  const cityDisplayOnLoad = document.getElementById("defaultCity");
+  cityDisplayOnLoad.addEventListener('load', nowShowingTemp);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
