09760af3d8402dc7c05cb063808eafbcf3076862
ashanti <ammanier2@gmail.com>
2022-12-17 15:22:08 -0500

    added temp color change

--- src/index.js
+++ src/index.js
@@ -2,87 +2,91 @@
 // import axios from "axios";
 // const { default: axios } = require("axios");
 
-const state = {temperature: 0};
-const tempValue = document.querySelector("#temp-value");
-state.temperature = Number(tempValue.innerText);
-const landscape = document.getElementById('#weather-landscape-section');
+// const state = {temperature: 75};
+// const tempVal = document.querySelector("#temp-value");
+// state.temperature = Number(tempVal.innerText);
+// let stateTemp = state["temperature"]
+const state = {temperature: 75};
+const tempVal = document.querySelector("#temp-value");
+state.temperature = Number(tempVal.innerText);
+let stateTemp = state["temperature"]
+const decButton = document.getElementById("decrease-temp");
+const incButton = document.getElementById("increase-temp");
 
-const tempRange = tempValue => {
-  if (num < 50) {
-    landscape.innerHTML = "./assests/cold-landscape.jpg"
-    return 'teal'
+const tempColor = (temp) => {
+  if (temp < 50){
+    return 'lightgreen';
+  }
+  else if (temp < 60){
+    return 'green';
+  }
+  else if (temp < 70){
+    return 'yellow';
+  }
+  else if (temp < 80){
+    return 'orange';
+  }
+  else {
+    return 'red';
   }
 }
 
-// const increaseTemp = () => {
-//   tempValue.innerText = state.temperature;
-//   state.temperature += 1;
-//   console.log("It's working!");
-// }
-// const decreaseTemp = () => {
-//   tempValue.innerText = state.temperature;
-//   state.temperature -= 1;
-//   console.log("It's working!");
-// }
-
-// const registerEventHandlers = () => {
-//   const decButton = document.querySelector("#decrease-temp");
-//   decButton.addEventListener("click", decreaseTemp);
-
-//   const incButton = document.querySelector("#increase-temp");
-//   incButton.addEventListener("click", increaseTemp);
-// };
-
-
-const registerEventHandlers = () => {
-  const decButton = document.querySelector("#decrease-temp");
+const registerEventHandlers = () => { 
   decButton.addEventListener("click", () => {
-
+    tempVal.innerHTML = stateTemp;
+    stateTemp -= 1;
+    tempVal.style.color = tempColor(Number(stateTemp));
   });
 
-  const incButton = document.querySelector("#increase-temp");
-  incButton.addEventListener("click", increaseTemp);
-};
+  incButton.addEventListener("click", () => {
+    tempVal.innerHTML = stateTemp;
+    stateTemp += 1;
+    tempVal.style.color = tempColor(Number(stateTemp));
+  });
 
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+ } 
 
-// API Calls
-const weatherLoc = (userInput) => {
-  const getLocation = () => {
-    axios
-    .get ('http://127.0.0.1:5000/location'), {
-      params: {
-        key: 'pk.b8aab1a85295b2c1f6e71a4ed20c3120',
-        q: userInput,
-        format: 'json'
-      }
-    }
-    .then (function(response){
-      let lat = response.data[0]["lat"];
-      let lon = response.data[0]["lon"];
-      getWeather(lat, lon);
-    })
-    .catch (function(error) {
-      console.error('error in getlocation!');
-    })
-  }
+ document.addEventListener("DOMContentLoaded", registerEventHandlers);
 
-  const getWeather = (lat, lon) => {
-    axios
-    .get ('http://127.0.0.1:5000/weather'), {
-      params: {
-        appid: '76d80595e856e8e068fcdd93241e2622',
-        lat: lat,
-        lon: lon
-      }
-    }
-    .then (function(response){
-      let tempK = response.data['main']['temp'];
-      let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
-      tempValue.innerText = tempF
-    })
-    .catch (function(error) {
-      console.error('error in getWeather');
-    })
-  }
-}
+
+
+// // API Calls
+// const weatherLoc = (userInput) => {
+//   const getLocation = () => {
+//     axios
+//     .get ('http://127.0.0.1:5000/location'), {
+//       params: {
+//         key: 'pk.b8aab1a85295b2c1f6e71a4ed20c3120',
+//         q: userInput,
+//         format: 'json'
+//       }
+//     }
+//     .then (function(response){
+//       let lat = response.data[0]["lat"];
+//       let lon = response.data[0]["lon"];
+//       getWeather(lat, lon);
+//     })
+//     .catch (function(error) {
+//       console.error('error in getlocation!');
+//     })
+//   }
+
+//   const getWeather = (lat, lon) => {
+//     axios
+//     .get ('http://127.0.0.1:5000/weather'), {
+//       params: {
+//         appid: '76d80595e856e8e068fcdd93241e2622',
+//         lat: lat,
+//         lon: lon
+//       }
+//     }
+//     .then (function(response){
+//       let tempK = response.data['main']['temp'];
+//       let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
+//       tempVal.innerText = tempF
+//     })
+//     .catch (function(error) {
+//       console.error('error in getWeather');
+//     })
+//   }
+// }
